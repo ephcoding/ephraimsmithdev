6cb2d49b457f80fbe6f6a8cffd39c4810f822c1a
ephcoding <email@ephcoding.com>
2021-08-20 11:32:03 -0500

    add BEM class names

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import MetaContainer from '@/components/MetaContainer';
 export default function HomePage({ posts }) {
 	return (
 		<MetaContainer>
-			<div id='ephcoding-landing-page'>
+			<div id='HomePage'>
 				<img
 					className='logo'
 					src='/images/ephcoding-text-logo.png'
@@ -12,24 +12,24 @@ export default function HomePage({ posts }) {
 					width={1000}
 				/>
 
-				<nav id='landing-page-nav' className='landing-page-nav'>
-					<ul className='landing-page-nav__ul'>
-						<li className='landing-page-nav__li'>
+				<nav id='HomePageNavMenu' className='main-nav'>
+					<ul className='main-nav__list'>
+						<li className='main-nav__item'>
 							<Link href='/blog'>
 								<a>blog</a>
 							</Link>
 						</li>
-						<li className='landing-page-nav__li'>
+						<li className='main-nav__item'>
 							<Link href='https://github.com/ephcoding'>
 								<a target='_blank'>github</a>
 							</Link>
 						</li>
-						<li className='landing-page-nav__li'>
+						<li className='main-nav__item'>
 							<Link href='https://twitter.com/ephcoding'>
 								<a target='_blank'>twitter</a>
 							</Link>
 						</li>
-						<li className='landing-page-nav__li'>
+						<li className='main-nav__item'>
 							<Link href='https://linkedin.com/in/ephraimjsmith'>
 								<a target='_blank'>linkedin</a>
 							</Link>
