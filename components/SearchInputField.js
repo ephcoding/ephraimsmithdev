e55ada23168e812e7c106aecdd1204b00b24c2e8
ephcoding <email@ephcoding.com>
2021-08-20 06:05:11 -0500

    rename component SearchInputField

--- components/SearchInputField.js
+++ components/SearchInputField.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import SearchResults from './SearchResults';
 
-export default function Search() {
+export default function SearchInputField() {
 	const [searchTerm, setSearchTerm] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
 
@@ -21,21 +21,21 @@ export default function Search() {
 	}, [searchTerm]);
 
 	return (
-		<div id='search-input' className='relative bg-gray-600 p-4'>
-			<div className='container mx-auto flex items-center justify-center md:justify-end'>
-				<div className='relative text-gray-600 w-72'>
+		<div id='SearchInputField' className=''>
+			<div className=''>
+				<div className=''>
 					<form>
 						<input
 							type='search'
 							name='search'
 							id='search'
-							className='bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-72'
+							className=''
 							value={searchTerm}
 							onChange={(e) => setSearchTerm(e.target.value)}
 							placeholder='Search Posts...'
 						/>
 
-						<FaSearch className='absolute top-0 right-0 text-black mt-3 mr-4' />
+						<FaSearch className='text-black' />
 					</form>
 				</div>
 			</div>
