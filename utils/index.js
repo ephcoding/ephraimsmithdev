d0597e5722d3210e389180dddb89f3221b0804c5
ephcoding <email@ephcoding.com>
2021-08-22 17:20:00 -0500

    add dynamic copyright year func to utils

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
 export const sortByDate = (a, b) => {
-  return new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
-}
+	return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
+};
+
+export const getCurrentYear = () => new Date(Date.now()).getFullYear();
