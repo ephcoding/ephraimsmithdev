a28f817db6364d75129ce1f468a58499fb29ecd6
ephcoding <email@ephcoding.com>
2021-08-22 17:21:32 -0500

    import new dynamic copyright year func

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,57 @@
 import Link from 'next/link';
+import { getCurrentYear } from '../utils/index';
 import MetaContainer from '@/components/MetaContainer';
 
 export default function HomePage({ posts }) {
 	return (
 		<MetaContainer>
-			<div id='HomePage'>
-				<img
-					className='logo'
-					src='/images/ephcoding-text-logo.png'
-					height={417}
-					width={1000}
-				/>
+			<div id='HomePage' className='homepage'>
+				<div className='container'>
+					<img
+						className='logo'
+						src='/images/ephcoding-text-logo.png'
+						height={417}
+						width={1000}
+					/>
+				</div>
 
 				<nav id='HomePageNavMenu' className='main-nav'>
 					<ul className='main-nav__list'>
 						<li className='main-nav__item'>
 							<Link href='/blog'>
-								<a>blog</a>
+								<a className='main-nav__item-link'>blog</a>
 							</Link>
 						</li>
 						<li className='main-nav__item'>
 							<Link href='https://github.com/ephcoding'>
-								<a target='_blank'>github</a>
+								<a className='main-nav__item-link' target='_blank'>
+									github
+								</a>
 							</Link>
 						</li>
 						<li className='main-nav__item'>
 							<Link href='https://twitter.com/ephcoding'>
-								<a target='_blank'>twitter</a>
+								<a className='main-nav__item-link' target='_blank'>
+									twitter
+								</a>
 							</Link>
 						</li>
 						<li className='main-nav__item'>
 							<Link href='https://linkedin.com/in/ephraimjsmith'>
-								<a target='_blank'>linkedin</a>
+								<a className='main-nav__item-link' target='_blank'>
+									linkedin
+								</a>
 							</Link>
 						</li>
 					</ul>
 				</nav>
 				{/* TODO: add Postman link */}
+				<footer>
+					<p className='copyright'>
+						<span className='copyright__symbol'>&copy;</span> {getCurrentYear()}{' '}
+						ephcoding.com
+					</p>
+				</footer>
 			</div>
 		</MetaContainer>
 	);
