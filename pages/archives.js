54b2b152848030eff00c10d8cf2916c7e44a1b79
ephcoding <email@ephcoding.com>
2021-08-24 22:37:47 -0500

    update styles for dynamic archive months

---
+++ pages/archives.js
@@ -0,0 +1,47 @@
+import { getPosts } from '@/lib/posts';
+import Link from 'next/link';
+import MetaContainer from '@/components/MetaContainer';
+import PostPreviewCard from '@/components/PostPreviewCard';
+
+export default function Archives({ posts }) {
+	let mostRecentMonth = new Date(posts[0].frontmatter.date);
+	let date, previousMonth, postMonth, monthName, isNewMonth;
+
+	return (
+		<MetaContainer>
+			<h2>{mostRecentMonth.toLocaleString('default', { month: 'long' })}</h2>
+			<div className='archive_cards'>
+				{posts.map((post, index) => {
+					date = new Date(post.frontmatter.date);
+					postMonth = date.getMonth();
+					monthName = date.toLocaleString('default', { month: 'long' });
+					isNewMonth = postMonth < previousMonth;
+					previousMonth = postMonth;
+					return (
+						<>
+							{isNewMonth && <h2>{monthName}</h2>}
+							{/* <PostPreviewCard key={index} post={post} /> */}
+							<div className='archive_card'>
+								<p className='row1'>
+									<Link href={`/blog/${post.slug}`}>
+										<a>{post.frontmatter.title}</a>
+									</Link>
+									<span>{post.frontmatter.date}</span>
+								</p>
+								<p className='row2'>{post.frontmatter.subtitle}</p>
+							</div>
+						</>
+					);
+				})}
+			</div>
+		</MetaContainer>
+	);
+}
+
+export async function getStaticProps() {
+	return {
+		props: {
+			posts: getPosts(),
+		},
+	};
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { getPosts } from '@/lib/posts';
-
 import Link from 'next/link';
 import MetaContainer from '@/components/MetaContainer';
 import PostPreviewCard from '@/components/PostPreviewCard';
@@ -7,35 +6,13 @@ import PostPreviewCard from '@/components/PostPreviewCard';
 export default function HomePage({ posts }) {
 	return (
 		<MetaContainer title='ephcoding.com | home'>
-			<img className='logo' src='/images/ephcoding-text-logo.png' />
-			{/* <nav className='homepage-nav'>
-				<Link href='/blog'>
-					<a className='homepage-nav__item'>blog</a>
-				</Link>
-				<Link href='https://github.com/ephcoding'>
-					<a className='homepage-nav__item' target='_blank'>
-						github
-					</a>
-				</Link>
-				<Link href='https://twitter.com/ephcoding'>
-					<a className='homepage-nav__item' target='_blank'>
-						twitter
-					</a>
-				</Link>
-				<Link href='https://linkedin.com/in/ephraimjsmith'>
-					<a className='homepage-nav__item' target='_blank'>
-						linkedin
-					</a>
-				</Link>
-			</nav> */}
-			{/* <div className='blog-posts__cards'>
-				<h1>Latest Posts</h1>
-				<div className=''>
-					{posts.map((post, index) => (
-						<PostPreviewCard key={index} post={post} />
-					))}
-				</div>
-			</div> */}
+			<div className='blog-posts__cards'>
+				{/* <div className=''> */}
+				{posts.map((post, index) => (
+					<PostPreviewCard key={index} post={post} />
+				))}
+				{/* </div> */}
+			</div>
 		</MetaContainer>
 	);
 }
