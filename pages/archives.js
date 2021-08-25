bf340a9d4c022a60eacdb7180a0bc621b16b6d07
ephcoding <email@ephcoding.com>
2021-08-25 18:50:44 -0500

    refactor Month divider logic

--- pages/archives.js
+++ pages/archives.js
@@ -1,28 +1,27 @@
+import { getDateMonthName } from '../utils';
 import { getPosts } from '@/lib/posts';
 import Link from 'next/link';
 import MetaContainer from '@/components/MetaContainer';
 import PostPreviewCard from '@/components/PostPreviewCard';
 
 export default function Archives({ posts }) {
-	let mostRecentMonth = new Date(posts[0].frontmatter.date);
-	let date, previousMonth, postMonth, monthName, isNewMonth;
+	let prevBlogPostMonth;
 
 	return (
 		<MetaContainer>
-			<h2 className='month_divider'>
-				{mostRecentMonth.toLocaleString('default', { month: 'long' })}
-			</h2>
 			<div className='archive_cards'>
 				{posts.map((post, index) => {
-					date = new Date(post.frontmatter.date);
-					postMonth = date.getMonth();
-					monthName = date.toLocaleString('default', { month: 'long' });
-					isNewMonth = postMonth < previousMonth;
-					previousMonth = postMonth;
+					const blogPostDate = new Date(post.frontmatter.date);
+					const blogPostMonth = blogPostDate.getMonth();
+					const blogPostMonthName = getDateMonthName(blogPostDate);
+					const isDiffMonth = blogPostMonth !== prevBlogPostMonth;
+					prevBlogPostMonth = blogPostMonth;
+
 					return (
 						<>
-							{isNewMonth && <h2 className='month_divider'>{monthName}</h2>}
-							{/* <PostPreviewCard key={index} post={post} /> */}
+							{isDiffMonth && (
+								<h2 className='month_divider'>{blogPostMonthName}</h2>
+							)}
 							<div className='archive_card'>
 								<p className='row1'>
 									<Link href={`/blog/${post.slug}`}>
