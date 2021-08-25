4ff2ddde94d61a91c5f4e3e12862bc185d245d85
ephcoding <email@ephcoding.com>
2021-08-24 20:13:18 -0500

    add new header with nav

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,21 @@
+import Link from 'next/link';
+
 export default function Header() {
-	return <div></div>;
+	return (
+		<header>
+			<Link href='/'>
+				<a>
+					<img src='/images/ephcoding-text-logo.png' />
+				</a>
+			</Link>
+			<nav>
+				<Link href='/archives'>
+					<a>archives</a>
+				</Link>
+				<Link href='/about'>
+					<a>about</a>
+				</Link>
+			</nav>
+		</header>
+	);
 }
