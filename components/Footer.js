b93242b742a9f464e114c4fc8ef03e041ac7458b
ephcoding <email@ephcoding.com>
2021-08-25 19:05:14 -0500

    clean haus'

--- components/Footer.js
+++ components/Footer.js
@@ -4,26 +4,6 @@ export default function Footer() {
 	return (
 		<footer className='footer'>
 			<p>&copy; {getCurrentYear()} Ephraim Smith</p>
-
-			{/* <h2>About</h2>
-			<div className='footer__content'>
-				<img
-					className='footer__pic'
-					src='/images/profile-pic.jpg'
-					alt='Ephraim Smith profile pic'
-				/>
-
-				<div className='footer__text'>
-					<p className='footer__about'>
-						dummie text. dummie text. dummie text. dummie text. dummie text.
-						dummie text. dummie text. dummie text. dummie text.
-					</p>
-					<p className='footer__copy'>
-						<span className='copyright__symbol'> &copy; </span>
-						{getCurrentYear()} Ephraim Smith
-					</p>
-				</div>
-			</div> */}
 		</footer>
 	);
 }
