5e2b5feffad969cf002f83e0c2a257ffc166a41f
eph <email@ephcoding.com>
2021-08-25 21:14:12 -0500

    update header classNames

--- components/Header.js
+++ components/Header.js
@@ -2,19 +2,21 @@ import Link from 'next/link';
 
 export default function Header() {
 	return (
-		<header>
-			<Link href='/'>
-				<a>
-					<img src='/images/ephcoding-text-logo.png' />
-				</a>
-			</Link>
-			<nav>
-				<Link href='/about'>
-					<a>about</a>
+		<header className='header'>
+			<nav className='header-nav'>
+				<Link href='/'>
+					<a className='header-nav__logo-link'>
+						<img className='header-nav__logo' src='/images/ephcoding-text-logo.png' />
+					</a>
 				</Link>
-				<Link href='/archives'>
-					<a>archives</a>
-				</Link>
+				<div className="sub-nav">
+					<Link href='/about'>
+						<a className='sub-nav__link'>about</a>
+					</Link>
+					<Link href='/archives'>
+						<a className='sub-nav__link'>archives</a>
+					</Link>
+				</div>
 			</nav>
 		</header>
 	);
