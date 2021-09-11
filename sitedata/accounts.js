628a036855a849b094ca51bdc459b2f90eca037e
ephcoding <ephcoding@gmail.com>
2021-09-11 08:27:29 -0500

    add accounts site data file

---
+++ sitedata/accounts.js
@@ -0,0 +1,31 @@
+export const accounts = [
+	{
+		label: 'Blog',
+		username: '',
+		url: '/',
+	},
+	// {
+	//   label: 'CodePen',
+	//   username: 'ephcoding',
+	//   url:
+	// },
+	{
+		label: 'Github',
+		username: 'ephcoding',
+		url: 'https://github.com/ephcoding',
+	},
+	{
+		label: 'LinkedIn',
+		username: 'ephraimjsmith',
+		url: 'https://www.linkedin.com/in/ephraimjsmith',
+	},
+	// {
+	//   username: 'ephcoding',
+	//   url: ''
+	// },
+	{
+		label: 'Twitter',
+		username: 'ephcoding',
+		url: 'https://www.twitter.com/ephcoding',
+	},
+];
