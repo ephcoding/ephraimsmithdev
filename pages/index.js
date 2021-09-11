deea840d085889dfbbbfb03a7a8f6698ab66f567
eph <ephraimjsmith@gmail.com>
2021-09-11 11:58:57 -0500

    trying to contain dat logo

--- pages/index.js
+++ pages/index.js
@@ -9,38 +9,18 @@ export default function HomePage({ posts }) {
 	return (
 		<MetaContainer title='ephcoding.com' keywords='' description=''>
 			<div className='container-fluid d-flex flex-column vh-100'>
-				<div className='flex-fill d-flex flex-column justify-content-center align-items-center'>
-					<img
-						src={'/ephcoding-icon-1000x1000.png'}
-						alt='ephcoding logo'
-						className='d-block mx-auto'
-						style={{ width: '75%' }}
-					/>
+				<div className='d-flex flex-column justify-content-center align-items-center flex-fill'>
+					<div className='flex-fill'>
+						<img
+							src={'/ephcoding-icon-1000x1000.png'}
+							alt='ephcoding logo'
+							className='d-inline-block mx-auto'
+							style={{ maxHeight: '100%' }}
+						/>
+					</div>
 
-					{/* <div className='d-flex' style={{ margin: '0 0 162px 0' }}>
-						<div className=''>
-							<Link href='/blog'>
-								<a className=''>blog</a>
-							</Link>
-						</div>
-						<div className=''>
-							<Link href={accounts.github.url}>
-								<a className=''>{accounts.github.label}</a>
-							</Link>
-						</div>
-						<div className='https://www.linkedin.com/in/ephraimjsmith'>
-							<Link href='/blog'>
-								<a className=''>linkedin</a>
-							</Link>
-						</div>
-						<div className='https://www.twitter.com/ephcoding'>
-							<Link href='/blog'>
-								<a className=''>twitter</a>
-							</Link>
-						</div>
-					</div> */}
-					{/* TODO: add Postman & CodePen */}
 					<NavMenu />
+					{/* TODO: add Postman & CodePen */}
 				</div>
 
 				<div>
