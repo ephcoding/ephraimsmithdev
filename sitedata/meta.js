48ed8c23741e8254a1f827304a90aba426b7ca04
ephcoding <ephcoding@gmail.com>
2021-09-11 08:26:04 -0500

    add meta info file

---
+++ sitedata/meta.js
@@ -0,0 +1,8 @@
+export const HOMEPAGE_DESCRIPTION =
+	"Ephraim Smith shares his experiences in building software applications and the lessons he's learned along the way.";
+export const HOMEPAGE_TITLE = 'ephcoding.com';
+export const HOMEPAGE_KEYWORDS =
+	'react native, mobile apps, mobile development, android, ios, coding, programming, software, software development';
+
+export const DESCRIPTION_BLOG = '';
+export const DESCRIPTION_BLOG = '';
