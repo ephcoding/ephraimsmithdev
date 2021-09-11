f168d259280019ac17bc7b31fdaf79d353094709
ephcoding <ephcoding@gmail.com>
2021-09-11 08:26:48 -0500

    start homepage refactor

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,52 @@
-import { getPosts } from '@/lib/posts';
 import Link from 'next/link';
+import Image from 'next/image';
 import MetaContainer from '@/components/MetaContainer';
-import PostPreviewCard from '@/components/PostPreviewCard';
+import Footer from '@/components/Footer';
+import { accounts } from '../sitedata/accounts';
+import { NavMenu } from '../components/NavMenu';
 
 export default function HomePage({ posts }) {
 	return (
-		<MetaContainer title='ephcoding.com | home'>
-			<div className='blog-posts__cards'>
-				{/* <div className=''> */}
-				{posts.map((post, index) => (
-					<PostPreviewCard key={index} post={post} />
-				))}
-				{/* </div> */}
-			</div>
-			<div className='row-end'>
-				<Link href='/archives'>
-					<a className='btn-nav_archives'>archives &gt;</a>
-				</Link>
+		<MetaContainer title='ephcoding.com' keywords='' description=''>
+			<div className='container-fluid d-flex flex-column vh-100'>
+				<div className='flex-fill d-flex flex-column justify-content-center align-items-center'>
+					<img
+						src={'/ephcoding-icon-1000x1000.png'}
+						alt='ephcoding logo'
+						className='d-block mx-auto'
+						style={{ width: '75%' }}
+					/>
+
+					{/* <div className='d-flex' style={{ margin: '0 0 162px 0' }}>
+						<div className=''>
+							<Link href='/blog'>
+								<a className=''>blog</a>
+							</Link>
+						</div>
+						<div className=''>
+							<Link href={accounts.github.url}>
+								<a className=''>{accounts.github.label}</a>
+							</Link>
+						</div>
+						<div className='https://www.linkedin.com/in/ephraimjsmith'>
+							<Link href='/blog'>
+								<a className=''>linkedin</a>
+							</Link>
+						</div>
+						<div className='https://www.twitter.com/ephcoding'>
+							<Link href='/blog'>
+								<a className=''>twitter</a>
+							</Link>
+						</div>
+					</div> */}
+					{/* TODO: add Postman & CodePen */}
+					<NavMenu />
+				</div>
+
+				<div>
+					<Footer />
+				</div>
 			</div>
 		</MetaContainer>
 	);
-}
-
-export async function getStaticProps() {
-	return {
-		props: {
-			posts: getPosts().slice(0, 5),
-		},
-	};
 }
