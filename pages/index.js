6ed8b8f9d1f36a15979c080cae371075b5bc821a
ephcoding <ephcoding@gmail.com>
2021-09-13 16:42:17 -0500

    fix landing logo width

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,16 @@ import { NavMenu } from '../components/NavMenu';
 export default function HomePage({ posts }) {
 	return (
 		<MetaContainer title='ephcoding.com' keywords='' description=''>
-			{/* <div className='container-fluid d-flex flex-column bg-dark vh-100'> */}
-			{/* <div> */}
 			<div className='d-flex flex-column justify-content-center align-items-center text-center flex-fill'>
 				<img
 					src={'/ephcoding-icon-1000x1000.png'}
 					alt='ephcoding logo'
 					className='d-inline-block mx-auto'
-					style={{ height: '45vmax', maxWidth: '600px' }}
+					style={{ height: '45vmax', maxHeight: '600px', width: 'auto' }}
 				/>
 				<NavMenu />
-
-				{/* TODO: add Postman & CodePen */}
 			</div>
-
-			{/* <div> */}
 			<Footer />
-			{/* </div> */}
-			{/* </div> */}
 		</MetaContainer>
 	);
 }
