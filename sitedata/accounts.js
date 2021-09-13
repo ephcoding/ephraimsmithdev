7349d7f59d9a1145c78e15aa69065298c36a5bc2
ephcoding <ephcoding@gmail.com>
2021-09-13 16:21:00 -0500

    add icons to account data file

--- sitedata/accounts.js
+++ sitedata/accounts.js
@@ -1,31 +1,58 @@
+import {
+	FaGithub,
+	FaLinkedin,
+	FaTwitter,
+	LinkedIn,
+	Twitter,
+} from 'react-icons/fa';
+
+const iconStyles = {
+	color: '#fff',
+	// height: '2.5rem',
+};
+
 export const accounts = [
 	{
-		label: 'Blog',
-		username: '',
+		label: 'BLOG',
+		icon: false,
+		reactIcon: null,
 		url: '/blog',
+		username: '',
+		target: '_self',
 	},
 	// {
 	//   label: 'CodePen',
-	//   username: 'ephcoding',
+	// icon: true,
+	// reactIcon:
 	//   url:
+	//   username: 'ephcoding',
 	// },
 	{
 		label: 'Github',
-		username: 'ephcoding',
+		icon: true,
+		reactIcon: <FaGithub style={iconStyles} />,
 		url: 'https://github.com/ephcoding',
+		username: 'ephcoding',
+		target: '_target',
 	},
 	{
 		label: 'LinkedIn',
-		username: 'ephraimjsmith',
+		icon: true,
+		reactIcon: <FaLinkedin style={iconStyles} />,
 		url: 'https://www.linkedin.com/in/ephraimjsmith',
+		username: 'ephraimjsmith',
+		target: '_target',
 	},
 	// {
-	//   username: 'ephcoding',
 	//   url: ''
+	//   username: 'ephcoding',
 	// },
 	{
 		label: 'Twitter',
-		username: 'ephcoding',
+		icon: true,
+		reactIcon: <FaTwitter style={iconStyles} />,
 		url: 'https://www.twitter.com/ephcoding',
+		username: 'ephcoding',
+		target: '_target',
 	},
 ];
