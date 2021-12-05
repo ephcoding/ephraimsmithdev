f9c346b7818c0485f50f9c6ce44c3b4dfda90de1
eph <ephcoding@gmail.com>
2021-12-05 13:35:19 -0600

    add branding component

---
+++ components/Branding.jsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import ecLogo from "../public/ephcoding-icon-1000x1000.png";
+
+const Branding = () => {
+  return <Image src={ecLogo} alt="ephcoding logo" />;
+};
+
+export default Branding;
