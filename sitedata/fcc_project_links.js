593a19fde640ffc262a3d4f6059e9fe4efa55720
ephcoding <ephcoding@gmail.com>
2021-12-05 07:25:19 -0600

    add link meta for FCC FE projects

---
+++ sitedata/fcc_project_links.js
@@ -0,0 +1,48 @@
+const GH_REPO_BASE = "https://github.com/ephcoding";
+const GH_PAGES_BASE = "https://ephcoding.github.io";
+
+const FCC_PROJECT_LINKS = [
+	// RESPONSIVE DESIGN CERTIFICATE
+	{
+		title: "Tribute Page",
+		repoLink: `${GH_REPO_BASE}/tribute-page`,
+		isLive: true,
+		liveLink: `${GH_PAGES_BASE}/tribute-page`,
+	},
+	{
+		title: "Survey Form",
+		repoLink: `${GH_REPO_BASE}/survey-form`,
+		isLive: true,
+		liveLink: `${GH_PAGES_BASE}/survey-form`,
+	},
+	{
+		title: "Product Landing Page",
+		repoLink: `${GH_REPO_BASE}/product-landing-page`,
+		isLive: true,
+		liveLink: `${GH_PAGES_BASE}/product-landing-page`,
+	},
+	{
+		title: "Technical Documentation Page",
+		repoLink: `${GH_REPO_BASE}/technical-documentation-page`,
+		isLive: true,
+		liveLink: `${GH_PAGES_BASE}/technical-documentation-page`,
+	},
+	{
+		title: "Portfolio Page",
+		repoLink: `${GH_REPO_BASE}/portfolio-page`,
+		isLive: true,
+		liveLink: `${GH_PAGES_BASE}/technical-documentation-page`,
+	},
+	// JAVASCRIPT ALGORITHMS & DATA STRUCTURES CERTIFICATE
+	// FRONT END DEVELOPMENT LIBRARIES CERTIFICATE
+	// DATA VISUALIZATION CERTIFICATE
+	// BACK END DEVELOPMENT & APIS CERTIFICATE
+	{
+		title: "Tribute Page",
+		repoLink: `${GH_REPO_BASE}/tribute-page`,
+		isLive: true,
+		liveLink: `${GH_PAGES_BASE}/tribute-page`,
+	},
+];
+
+export default FCC_PROJECT_LINKS;
