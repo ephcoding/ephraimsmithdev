c877c0caf3c7d8289534afa4922164a0441a0ff0
ephcoding <ephcoding@gmail.com>
2021-12-05 07:59:22 -0600

    swap out fcc link components for fcc project card components

--- pages/projects/freecodecamp/index.jsx
+++ pages/projects/freecodecamp/index.jsx
@@ -1,21 +1,24 @@
 import MetaContainer from "@/components/MetaContainer";
 import Link from "next/link";
 import FCC_PROJECT_LINKS from "../../../sitedata/fcc_project_links.js";
-import FccProjectLink from "@/components/FccProjectLink";
+import FccProjectCard from "@/components/FccProjectCard";
 import MainNav from "@/components/MainNav";
 
 export default function FreeCodeCamp() {
 	return (
 		<MetaContainer>
 			<h1>FreeCodeCamp Projects</h1>
-			<ul>
-				{FCC_PROJECT_LINKS.map(link => {
-					return <FccProjectLink key={link.repoLink} projectMeta={link} />;
+			<ul className='fcc-projects'>
+				{FCC_PROJECT_LINKS.map(project => {
+					return (
+						<FccProjectCard key={project.repoLink} projectMeta={project} />
+					);
 				})}
 			</ul>
 
 			<MainNav />
 
+			{/* DEV NAV */}
 			<div>
 				<Link href='/projects'>
 					<a>{`<< Back`}</a>
