c7db0e83681e39d643a8043695b48ec47d4acf77
eph <ephcoding@gmail.com>
2021-12-05 13:35:31 -0600

    add branding comp to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,13 @@
+import Branding from "./Branding";
 import MainNav from "./MainNav";
 import SocialNav from "./SocialNav";
 
 export default function Header() {
-	return (
-		<header className='header'>
-			<MainNav />
-			<SocialNav />
-		</header>
-	);
+  return (
+    <header className="header">
+      <Branding />
+      <MainNav />
+      <SocialNav />
+    </header>
+  );
 }
