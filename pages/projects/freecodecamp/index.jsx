b15b966528e7dc1a587368c07687453d5651da75
ephcoding <ephcoding@gmail.com>
2021-12-05 12:11:59 -0600

    replace main nav with header (main nav & social nav)

--- pages/projects/freecodecamp/index.jsx
+++ pages/projects/freecodecamp/index.jsx
@@ -2,12 +2,12 @@ import MetaContainer from "@/components/MetaContainer";
 import Link from "next/link";
 import FCC_PROJECT_LINKS from "../../../sitedata/fcc_project_links.js";
 import FccProjectCard from "@/components/FccProjectCard";
-import MainNav from "@/components/MainNav";
+import Header from "@/components/Header";
 
 export default function FreeCodeCamp() {
 	return (
 		<MetaContainer>
-			<MainNav />
+			<Header />
 			<h1>FreeCodeCamp Projects</h1>
 			<ul className='fcc-projects'>
 				{FCC_PROJECT_LINKS.map(project => {
