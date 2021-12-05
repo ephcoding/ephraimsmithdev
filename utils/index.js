de095023e41da3e3fffad7438bfd7ada15a78140
eph <ephcoding@gmail.com>
2021-12-05 13:35:43 -0600

    86 white space

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,7 @@
 export const sortByDate = (a, b) => {
-	// TODO: understand .sort(sortByDate) logic
-	return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
+  // TODO: understand .sort(sortByDate) logic
+  return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
 };
-
 export const getCurrentYear = () => new Date(Date.now()).getFullYear();
-
 export const getDateMonthName = (date) =>
-	date.toLocaleString('default', { month: 'long' });
+  date.toLocaleString("default", { month: "long" });
