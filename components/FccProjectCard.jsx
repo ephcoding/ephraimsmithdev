bc6ceb40c55cec39797665d4540cc0944cdb9b58
ephcoding <ephcoding@gmail.com>
2021-12-05 08:20:50 -0600

    relo project h2 to project card comp

--- components/FccProjectCard.jsx
+++ components/FccProjectCard.jsx
@@ -1,9 +1,11 @@
 import FccProjectLink from "@/components/FccProjectLink";
 
 const FccProjectCard = ({ projectMeta }) => {
+	const { title } = projectMeta;
 	return (
 		<li>
 			<div className='fcc-projects__card'>
+				<h2>{title}</h2>
 				<FccProjectLink projectMeta={projectMeta} />
 			</div>
 		</li>
