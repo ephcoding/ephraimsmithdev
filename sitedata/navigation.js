b9be105cb685ad90badf7502650f081ed7ad8092
ephcoding <ephcoding@gmail.com>
2021-12-06 12:47:14 -0600

    refactor site data

--- sitedata/navigation.js
+++ sitedata/navigation.js
@@ -1,11 +1,26 @@
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
+export const MAIN_NAV = [
+	{
+		LABEL: "home",
+		URL: "/",
+	},
+	{
+		LABEL: "blog",
+		URL: "/blog",
+	},
+	{
+		LABEL: "projects",
+		URL: "/projects",
+	},
+];
+
 const iconStyles = {
 	color: "$black",
 	height: "1rem",
 };
 
-export const SOCIAL_NAV_LINKS = [
+export const SOCIAL_NAV = [
 	{
 		label: "Github",
 		reactIcon: <FaGithub style={iconStyles} />,
