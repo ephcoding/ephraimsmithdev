ea7fe83af4503c47e36017d26b499355622425ce
ephcoding <ephcoding@gmail.com>
2021-12-06 07:25:56 -0600

    break FCC projects into certificate sections

--- pages/projects/freecodecamp/index.jsx
+++ pages/projects/freecodecamp/index.jsx
@@ -1,25 +1,33 @@
 import MetaContainer from "@/components/MetaContainer";
 import Link from "next/link";
-import FCC_PROJECT_LINKS from "../../../sitedata/fcc_project_links.js";
+import FCC_PROJECT_META from "../../../sitedata/fcc_project_links.js";
 import FccProjectCard from "@/components/FccProjectCard";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { PAGE_META_DATA } from "../../../sitedata/meta_data";
 
-export default function FreeCodeCamp(meta) {
+export default function FreeCodeCamp() {
+	const { freecodecamp } = PAGE_META_DATA;
+
 	return (
-		<MetaContainer meta={meta}>
+		<MetaContainer meta={freecodecamp}>
 			<div className='page freecodecamp-page'>
 				<Header />
 				<main className='content'>
 					<h1>FreeCodeCamp Projects</h1>
-					<ul className='fcc-projects'>
-						{FCC_PROJECT_LINKS.map(project => {
-							return (
-								<FccProjectCard key={project.repoLink} projectMeta={project} />
-							);
-						})}
-					</ul>
+
+					{FCC_PROJECT_META.map(({ certificate, projects }) => {
+						const projectCards = projects.map(project => (
+							<FccProjectCard key={project.repoLink} projectMeta={project} />
+						));
+
+						return (
+							<section>
+								<h2 key={certificate}>{certificate}</h2>
+								<ul className='fcc-projects'>{projectCards}</ul>
+							</section>
+						);
+					})}
 				</main>
 				<Footer />
 			</div>
