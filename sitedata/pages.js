27af7e6079fc5c0cf18debad5513c6513a5d471a
ephcoding <ephcoding@gmail.com>
2021-12-06 13:53:49 -0600

    make page titles "branding-friendly" (shorter)

--- sitedata/pages.js
+++ sitedata/pages.js
@@ -1,25 +1,25 @@
 export const PAGE_META_DATA = {
 	blog: {
-		title: "Blog Posts by Ephraim Smith",
+		title: "Blog Posts",
 		description: "",
 		keywords:
 			"Blog, Blogging, Blog Posts, Software Blog, Javascript, React, React Native, Node.js, APIs",
 	},
 	freecodecamp: {
-		title: "FreeCodeCamp Projects Built By Ephraim Smith",
+		title: "FreeCodeCamp Projects",
 		description: "",
 		keywords:
 			"freeCodeCamp, Responsive Design, JavaScript, React, D3.js, d3js, D3, HTML, CSS, HTML5, CSS3",
 	},
 	home: {
-		title: "ephcoding.com | Ephraim Smith's Adventures in Software",
+		title: "Eph Coding",
 		description:
 			"ephcoding.com is home to Ephraim Smith and his documented adventures in software",
 		keywords:
 			"react native, mobile apps, mobile development, android, ios, coding, programming, software, software development",
 	},
 	projects: {
-		title: "Active Software Projects Built by Ephraim Smith",
+		title: "Active Projects",
 		description: "",
 		keywords: "",
 	},
