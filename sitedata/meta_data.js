e4847badbf0531d1f992554d140e89119f5aac48
ephcoding <ephcoding@gmail.com>
2021-12-06 10:31:30 -0600

    start adding to site meta

--- sitedata/meta_data.js
+++ sitedata/meta_data.js
@@ -1,23 +1,25 @@
 export const PAGE_META_DATA = {
 	blog: {
-		title: "Blog",
+		title: "Blog Posts by Ephraim Smith",
 		description: "",
-		keywords: "",
+		keywords:
+			"Blog, Blogging, Blog Posts, Software Blog, Javascript, React, React Native, Node.js, APIs",
 	},
 	freecodecamp: {
-		title: "FreeCodeCamp Projects",
+		title: "FreeCodeCamp Projects Built By Ephraim Smith",
 		description: "",
-		keywords: "",
+		keywords:
+			"freeCodeCamp, Responsive Design, JavaScript, React, D3.js, d3js, D3, HTML, CSS, HTML5, CSS3",
 	},
 	home: {
-		title: "ephcoding.com",
+		title: "ephcoding.com | Ephraim Smith's Adventures in Software",
 		description:
 			"ephcoding.com is home to Ephraim Smith and his documented adventures in software",
 		keywords:
 			"react native, mobile apps, mobile development, android, ios, coding, programming, software, software development",
 	},
 	projects: {
-		title: "Projects",
+		title: "Active Software Projects Built by Ephraim Smith",
 		description: "",
 		keywords: "",
 	},
