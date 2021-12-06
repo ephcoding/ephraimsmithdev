326fdadecfd7ca3dc4b0f08c6831ffc6c266f27a
ephcoding <ephcoding@gmail.com>
2021-12-06 00:19:11 -0600

    update blog post imports

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,54 +1,49 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import marked from 'marked';
-import Footer from '@/components/Footer';
-import { NavBtn } from '@/components/NavBtn';
-import Link from 'next/link';
-import MetaContainer from '@/components/MetaContainer';
-import { NavBlogPost } from '@/components/NavMenuMain';
-import { NavMenuMain } from '@/components/NavMenuMain';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import marked from "marked";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { NavBtn } from "@/components/NavBtn";
+import Link from "next/link";
+import MetaContainer from "@/components/MetaContainer";
 
 export default function BlogPost({
 	frontmatter: { title, subtitle, date, author, keywords, description },
 	content,
 	slug,
 }) {
+	const meta = { title, description, keywords };
 	return (
-		<MetaContainer title={title} keywords={keywords} description={description}>
-			{/* <Head>
-				<title>{title}</title>
-				<meta name='keywords' content={keywords} />
-				<meta name='description' content={description} />
-				<link rel='icon' href='/favicon.ico' />
-			</Head> */}
-
-			<article id={title} className='flex-fill pt-3'>
-				<h1 className='h1'>
-					<strong>{title}</strong>
-				</h1>
-				<h2 className=''>{subtitle}</h2>
-				<p className='fs-6'>by {author}</p>
-				<p className='text-end'>{date}</p>
-				<div className='separator'></div>
-				{/* <div className=''> */}
-				<div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
-				{/* </div> */}
-
-				<NavMenuMain />
-			</article>
-
-			<Footer />
+		<MetaContainer meta={meta}>
+			<div className='blog-post'>
+				<Header />
+				<main className='blog-post__content'>
+					<article id={title} className='flex-fill pt-3'>
+						<h1 className='h1'>
+							<strong>{title}</strong>
+						</h1>
+						<h2 className=''>{subtitle}</h2>
+						<p className='fs-6'>by {author}</p>
+						<p className='text-end'>{date}</p>
+						<div className='separator'></div>
+						{/* <div className=''> */}
+						<div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+						{/* </div> */}
+					</article>
+				</main>
+				<Footer />
+			</div>
 		</MetaContainer>
 	);
 }
 
 export async function getStaticPaths() {
-	const files = fs.readdirSync(path.join('posts'));
+	const files = fs.readdirSync(path.join("posts"));
 
-	const paths = files.map((filename) => ({
+	const paths = files.map(filename => ({
 		params: {
-			slug: filename.replace('.md', ''),
+			slug: filename.replace(".md", ""),
 		},
 	}));
 
@@ -60,8 +55,8 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params: { slug } }) {
 	const markdownWithMeta = fs.readFileSync(
-		path.join('posts', slug + '.md'),
-		'utf-8'
+		path.join("posts", slug + ".md"),
+		"utf-8"
 	);
 
 	const { data: frontmatter, content } = matter(markdownWithMeta);
