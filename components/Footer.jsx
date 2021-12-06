2845dcd246eaf75c8274c84b56c9a505218c340d
ephcoding <ephcoding@gmail.com>
2021-12-06 13:43:38 -0600

    update comp to include social cta

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,8 @@
-import { getCurrentYear } from "../utils/date";
-import Link from "next/link";
 import SocialNav from "@/components/SocialNav";
 
 export default function Footer() {
 	return (
 		<footer className='footer'>
-			<span>contact: ephraim@ephcoding.com</span>
 			<SocialNav />
 		</footer>
 	);
