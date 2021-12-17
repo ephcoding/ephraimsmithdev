67325ffa2475e3acd40d350dd5d200a2053a1230
ephcoding <ephcoding@gmail.com>
2021-12-16 23:26:26 -0600

    add Active Builds

--- pages/builds/index.jsx
+++ pages/builds/index.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { PAGE_META } from "../../sitedata/meta";
+import { FaFreeCodeCamp } from "react-icons/fa";
 
 export default function ProjectsPage() {
 	const { builds } = PAGE_META;
@@ -12,20 +13,20 @@ export default function ProjectsPage() {
 			<div className='page builds-page'>
 				<Header />
 				<main className='content'>
-					<h1>{builds}</h1>
+					<h1>
+						<span className='blue'>Active</span> Builds
+					</h1>
 					<div>
-						<Link href='/builds/tornado-action'>
-							<a>Tornado Action</a>
+						<Link href='https://www.tornadoaction.com'>
+							<a target='_blank'>Tornado Action</a>
 						</Link>
 					</div>
 					<div>
-						<Link href='/builds/freecodecamp'>
-							<a>FreeCodeCamp</a>
-						</Link>
-					</div>
-					<div>
-						<Link href='/'>
-							<a>HOME</a>
+						<Link href='https://www.freecodecamp.org/ephcoding'>
+							<div className='project'>
+								<FaFreeCodeCamp size={20} title='freeCodeCamp' />
+								<a target='_blank'>FreeCodeCamp</a>
+							</div>
 						</Link>
 					</div>
 				</main>
