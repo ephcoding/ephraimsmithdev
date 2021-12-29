f2bccfe5e8e29138d976abe19b8053d2966bc7a8
ephcoding <ephcoding@gmail.com>
2021-12-29 16:54:55 -0600

    remove code relocated to Page comp

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,36 +1,27 @@
-import Meta from "@/components/Page";
+import Page from "@/components/Page";
 import Link from "next/link";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import { PAGE_INFO } from "../../sitedata/page_info";
 
 export default function ProjectsPage() {
-	const { builds } = PAGE_INFO;
+	const { projects } = PAGE_INFO;
 
 	return (
-		<Meta meta={builds}>
-			<div className='page builds-page'>
-				<Header />
-				<main className='content'>
-					<h1>{builds}</h1>
-					<div>
-						<Link href='/builds/tornado-action'>
-							<a>Tornado Action</a>
-						</Link>
-					</div>
-					<div>
-						<Link href='/builds/freecodecamp'>
-							<a>FreeCodeCamp</a>
-						</Link>
-					</div>
-					<div>
-						<Link href='/'>
-							<a>HOME</a>
-						</Link>
-					</div>
-				</main>
-				<Footer />
+		<Page pageInfo={projects}>
+			<div>
+				<Link href='/builds/tornado-action'>
+					<a>Tornado Action</a>
+				</Link>
 			</div>
-		</Meta>
+			<div>
+				<Link href='/builds/freecodecamp'>
+					<a>FreeCodeCamp</a>
+				</Link>
+			</div>
+			<div>
+				<Link href='/'>
+					<a>HOME</a>
+				</Link>
+			</div>
+		</Page>
 	);
 }
