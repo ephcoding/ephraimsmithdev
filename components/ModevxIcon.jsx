b2e76ed74c23fcc8c81b62ca3d3e226f85c22adf
ephcoding <ephcoding@gmail.com>
2021-12-29 10:55:26 -0600

    add svg as component

---
+++ components/ModevxIcon.jsx
@@ -0,0 +1,91 @@
+const ModevxIcon = ({ height, width, fill }) => {
+	return (
+		<svg
+			xmlns:dc='http://purl.org/dc/elements/1.1/'
+			xmlns:cc='http://creativecommons.org/ns#'
+			xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+			xmlns:svg='http://www.w3.org/2000/svg'
+			xmlns='http://www.w3.org/2000/svg'
+			xmlns:sodipodi='http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd'
+			xmlns:inkscape='http://www.inkscape.org/namespaces/inkscape'
+			width={width}
+			height={height}
+			viewBox='0 0 135.46666 135.46667'
+			version='1.1'
+			id='svg3274'
+			inkscape:version='1.0.1 (c497b03c, 2020-09-10)'
+			sodipodi:docname='badge-solid-white.svg'
+			inkscape:export-filename='/Users/eph/_modevx/_new-assets/badge-solid-white.png'
+			inkscape:export-xdpi='96'
+			inkscape:export-ydpi='96'
+		>
+			<metadata id='metadata22'>
+				<rdf:RDF>
+					<cc:Work rdf:about=''>
+						<dc:format>image/svg+xml</dc:format>
+						<dc:type rdf:resource='http://purl.org/dc/dcmitype/StillImage' />
+						<dc:title></dc:title>
+					</cc:Work>
+				</rdf:RDF>
+			</metadata>
+			<sodipodi:namedview
+				id='namedview3276'
+				pagecolor='#666666'
+				bordercolor='#eeeeee'
+				borderopacity='1'
+				inkscape:pageshadow='0'
+				inkscape:pageopacity='0'
+				inkscape:pagecheckerboard='true'
+				inkscape:document-units='px'
+				showgrid='false'
+				units='px'
+				inkscape:zoom='0.49762069'
+				inkscape:cx='218.79739'
+				inkscape:cy='283.45191'
+				inkscape:window-width='1440'
+				inkscape:window-height='900'
+				inkscape:window-x='0'
+				inkscape:window-y='0'
+				inkscape:window-maximized='0'
+				inkscape:current-layer='layer1'
+				inkscape:document-rotation='0'
+				inkscape:showpageshadow='false'
+				showborder='true'
+			/>
+			<defs id='defs3271'>
+				<filter
+					inkscape:collect='always'
+					style='color-interpolation-filters:sRGB'
+					id='filter1508'
+					x='-1.1594223e-06'
+					width='1.0000023'
+					y='-1.243521e-06'
+					height='1.0000025'
+				>
+					<feGaussianBlur
+						inkscape:collect='always'
+						stdDeviation='3.5859e-05'
+						id='feGaussianBlur1510'
+					/>
+				</filter>
+			</defs>
+			<g inkscape:label='Layer 1' inkscape:groupmode='layer' id='layer1'>
+				<g
+					id='g2470'
+					transform='matrix(1.29817,0,0,-1.29817,115.91362,22.811459)'
+					style='fill:{fill};fill-opacity:1;stroke:none;stroke-width:1.4796;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;filter:url(#filter1508)'
+					inkscape:export-xdpi='96'
+					inkscape:export-ydpi='96'
+				>
+					<path
+						d='m 0,0 h -20.061 l -14.358,-22.785 -7.978,14.209 h -22.038 l 15.502,-22.948 -25.295,-37.684 h 21.581 l 15.948,25.45 8.283,-15.046 h 23.708 l -16.945,26.216 z'
+						style={`fill:${fill};fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:1.4796;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
+						id='path2468'
+					/>
+				</g>
+			</g>
+		</svg>
+	);
+};
+
+export default ModevxIcon;
