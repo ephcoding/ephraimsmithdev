39f6021a46e3074201cea76344a0878cdbe1b0a3
ephcoding <ephcoding@gmail.com>
2021-12-30 16:08:48 -0600

    un-comment project meta for dev testing

--- js/site_data.js
+++ js/site_data.js
@@ -71,40 +71,40 @@ export const PAGES = {
 	},
 };
 export const PROJECTS = [
-	// {
-	// 	description:
-	// 		'Short for "Algo-Trading Strategy", ALSTR will be a full platform for managing my personal algo-trading strategies.  Learning all the things with this project.  Frontend, backend, APIs, security, data storage and caching, and cloud architecture.',
-	// 	name: "ALSTR",
-	// 	urls: {
-	// 		appStore: null,
-	// 		repo: "https://www.github.com/ephraimsmith/alstr",
-	// 		site: null,
-	// 	},
-	// },
-	// {
-	// 	description:
-	// 		"Visual-first web app for all things tornado-related.  I wanted to build a UI-heavy, engaging application that was both fun and practical.  To keep costs down I honed in on free/publicly available datasets.  Between NOAA, NASA, the National Weather Service, and the Storm Prediction Center, it's APIs-for-days...",
-	// 	name: "Tornado Action",
-	// 	urls: {
-	// {appStore: "https://www.tornadoaction.com"},
-	// { repo: "https://www.github.com/ephraimsmith/tornado-action" },
-	// {site: ""}
-
-	// 	],
-	// },
-	// {
-	// 	description:
-	// 		"ROVERCAM's a cross-platform mobile app built with React Native that gives users access to thousands of photos taken on Mars by the 4 Mars Rovers: Curiosity, Opportunity, Perseverance, and Spirit.",
-	// 	name: "ROVERCAM",
-	// 	urls: {
-	// {appStore: ""},
-	// { repo: "https://www.github.com/modevx/rovercam" }, { site: "" }],
-	// 	// site: "https://www.rovercamapp.com",
-	// },
+	{
+		description:
+			'Short for "Algo-Trading Strategy", ALSTR will be a full platform for managing my personal algo-trading strategies.  Learning all the things with this project.  Frontend, backend, APIs, security, data storage and caching, and cloud architecture.',
+		name: "ALSTR",
+		urls: {
+			appStore: null,
+			repo: "https://www.github.com/ephraimsmith/alstr",
+			site: null,
+		},
+	},
+	{
+		description:
+			"Visual-first web app for all things tornado-related.  I wanted to build a UI-heavy, engaging application that was both fun and practical.  To keep costs down I honed in on free/publicly available datasets.  Between NOAA, NASA, the National Weather Service, and the Storm Prediction Center, it's APIs-for-days...",
+		name: "Tornado Action",
+		urls: {
+			appStore: "https://www.tornadoaction.com",
+			repo: "https://www.github.com/ephraimsmith/tornado-action",
+			site: "",
+		},
+	},
+	{
+		description:
+			"ROVERCAM's a cross-platform mobile app built with React Native that gives users access to thousands of photos taken on Mars by the 4 Mars Rovers: Curiosity, Opportunity, Perseverance, and Spirit.",
+		name: "ROVERCAM",
+		urls: {
+			appStore: "",
+			repo: "https://www.github.com/modevx/rovercam",
+			site: "https://www.rovercamapp.com",
+		},
+	},
 	{
 		bgImgClass: "bg_tornado-at-farm",
 		description:
-			"REST API that will allow developers to access over 65,000 historical tornado events from 1950-2019.  The Storm Prediction Center provides this data in CSV format.  The plan is to stash this mountain of data in a PostgreSQL database in the cloud.  Eventually I hope to add dyamic updates that will feed the VORTX database as newly published storm reports are released from the National Weather Service.",
+			"VORTX is a REST API that will give developers access to over 65,000 historical tornado events.  The Storm Prediction Center provides severe weather data reports in CSV format.",
 		name: "VORTX",
 		urls: {
 			appStore: "",
