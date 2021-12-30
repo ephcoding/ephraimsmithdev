dece33b3b4551f13c30d9876093be31ffd489095
ephcoding <ephcoding@gmail.com>
2021-12-30 16:02:04 -0600

    laydown init project card styles

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,13 +1,15 @@
 import CardLinks from "@/components/CardLinks";
 
 const Card = ({ cardMeta }) => {
-	const { description, name, URLs } = cardMeta;
+	const { bgImgClass, description, name, urls } = cardMeta;
 
 	return (
-		<li className='card'>
-			<h2>{name}</h2>
-			<p>{description}</p>
-			<CardLinks links={URLs} />
+		<li className={`card ${bgImgClass}`}>
+			<div className='card-text'>
+				<h2>{name}</h2>
+				<p>{description}</p>
+			</div>
+			<CardLinks links={urls} />
 		</li>
 	);
 };
