812e2ad99c64d6618a1521ea3723b7eb6b80e0e1
ephcoding <ephcoding@gmail.com>
2022-01-01 16:33:15 -0600

    wrap up tools init list

--- js/site_data.js
+++ js/site_data.js
@@ -30,17 +30,17 @@ export const NAV_ITEMS = [
 	},
 	{
 		label: "github",
-		url: "https://github.com/ephraimsmith",
+		url: "https://github.com/ephcoding",
 		icon: <FaGithub size={20} title='github' />,
 	},
 	{
 		label: "twitter",
-		url: "https://www.twitter.com/_ephraimsmith",
+		url: "https://www.twitter.com/ephcoding",
 		icon: <FaTwitter size={20} title='twitter' />,
 	},
 	{
 		label: "linkedin",
-		url: "https://www.linkedin.com/in/ephraimjsmith",
+		url: "https://www.linkedin.com/in/ephcoding",
 		icon: <FaLinkedin size={20} title='linkedin' />,
 	},
 ];
@@ -139,54 +139,53 @@ export const TOOLS = [
 	{
 		name: "Architecture Diagrams (AWS)",
 		url: "https://aws.amazon.com/architecture/reference-architecture-diagrams/?whitepapers-main.sort-by=item.additionalFields.sortDate&whitepapers-main.sort-order=desc&awsf.whitepapers-tech-category=*all&awsf.whitepapers-industries=*all&solutions-all.sort-by=item.additionalFields.sortDate&solutions-all.sort-order=desc",
-		description: "",
+		description: "awesome for understanding how systems tie together.",
 	},
 	{
 		name: "Awesome CTO",
 		url: "https://github.com/kuchin/awesome-cto",
 		description:
-			"A curated and opinionated list of resources for Chief Technology Officers and VP R&D",
+			"A curated and opinionated list of resources for Chief Technology Officers and VP R&D.",
 	},
 	{
 		name: "AwesomeOpenSource",
-		url: "",
-		description: "",
+		url: "https://awesomeopensource.com",
+		description:
+			"if you're looking for open source projects to get involved in, this is the mother of all rabit holes..",
 	},
 	{
 		name: "Brad Traversy",
 		url: "https://www.traversymedia.com",
-		description: "",
+		description:
+			"You can start a career in software with Brad's courses alone.  No.  Seriously.",
 	},
 	{
 		name: "Can I Use",
-		url: "",
-		description: "",
+		url: "https://caniuse.com",
+		description:
+			'"Can I use provides up-to-date browser support tables for support of front-end web technologies on desktop and mobile web browsers."',
 	},
 	{
 		name: "Canva",
 		url: "https://www.canva.com",
-		description: "",
+		description:
+			"Free asset-creation platform & design lessons.  I use Canva exclusively for anything graphics-related.",
 	},
 	{
 		name: "createapp.dev",
 		url: "https://createapp.dev/",
 		description:
-			"generates project build configs based on your selected packages",
+			"This might be one of the coolest tools I've stumbled upon.  createapp.dev generates your project's config file based on the packages you select.",
 	},
 	{
 		name: "CSS-Tricks",
-		url: "",
-		description: "",
+		url: "https://css-tricks.com/",
+		description: "If you have a CSS question, start here.",
 	},
 	{
-		name: "Design Patterns",
-		url: "https://refactoring.guru/design-patterns",
-		description: "",
-	},
-	{
-		name: "devhints.io cheat sheets",
-		url: "",
-		description: "",
+		name: "devhints.io cheatsheets",
+		url: "https://devhints.io/",
+		description: "Kick-ass cheatsheets for a pile of topics.",
 	},
 	{
 		name: "EZGif",
@@ -195,63 +194,77 @@ export const TOOLS = [
 	},
 	{
 		name: "Flexbox Froggy",
-		url: "",
-		description: "",
+		url: "https://flexboxfroggy.com/",
+		description: "Game for learning CSS Flexbox.  Effective.",
 	},
 	{
 		name: "FreeCodeCamp",
 		url: "https://freecodecamp.org",
-		description: "",
+		description:
+			"If you're new to software and overwhelmed about where to start, check out freeCodeCamp.  It's basically a MERN stack bootcamp with some data visualizations, analytics, and Python thrown in.  Extensive catolog of interview prep challenges too.",
 	},
 	{
 		name: "Grid Garden",
-		url: "",
-		description: "",
+		url: "https://cssgridgarden.com/",
+		description: "Game for learning CSS Grid.  Like Flexbox Froggy, effective.",
 	},
 	{
 		name: "Lighthouse (Google)",
-		url: "",
-		description: "",
+		url: "https://pagespeed.web.dev/?utm_source=psi&utm_medium=redirect",
+		description: "Measure your websites perform with Lighthouse from Google.",
 	},
 	{
 		name: "NASA APIs",
-		url: "",
-		description: "",
+		url: "https://api.nasa.gov/#apod",
+		description:
+			"NASA's most popular open datasets in one place.  All kinds of cool data to use for those frontend framework / fetch-and-display projects.",
 	},
 	{
 		name: "NOAA Big Data Program",
 		url: "https://www.noaa.gov/organization/information-technology/list-of-big-data-program-datasets",
-		description: "",
+		description:
+			"Grab a beer / coffee and a sandwich.  This data rabbit hole is deeep.",
 	},
 	{
 		name: "OOCSS",
-		url: "",
-		description: "",
+		url: "https://github.com/stubbornella/oocss/wiki#separate-container-and-content",
+		description: "An object-oriented approach to writing CSS.",
 	},
 	{
 		name: "openbase.com",
 		url: "https://openbase.com/",
-		description: "",
+		description:
+			'"Compare open-source packages with powerful metrics and user reviews."',
+	},
+	{
+		name: "refactoring.guru (design patterns)",
+		url: "https://refactoring.guru/design-patterns",
+		description:
+			'Another one of those "how did I not know this existed?" sites.  Even if you\'re just getting started in software, the concepts here are worth knowing at a high-level.',
 	},
 	{
 		name: "SMACSS",
-		url: "",
-		description: "",
+		url: "http://smacss.com/",
+		description:
+			'"A flexible (CSS) guide to developing sites small and large."',
 	},
 	{
 		name: "Twelve-Factor App",
 		url: "https://12factor.net/",
-		description: "serious about building a SaaS product?  start here.",
+		description:
+			"Serious about building a SaaS product?  this is required reading.",
 	},
 	{
 		name: "Twitter Card Validator",
 		url: "https://cards-dev.twitter.com/validator",
-		description: "",
+		description:
+			"Cool tool for previewing what your meta thumbnails will look like in a Tweet.",
 	},
 	{
 		name: "W3Schools",
 		url: "https://www.w3schools.com",
-		description: "",
+		description:
+			"Huge resource with references, lessons, and practice problems for all kinds of languages.",
 	},
 ];
 export const FCC_PROJECTS = [
