820277830651b975429de00df1c148e5a8e230f6
Ephraim <admin@ephraimsmith.dev>
2022-01-01 18:38:34 -0600

    install pkgs using npm

--- components/Page.jsx
+++ components/Page.jsx
@@ -3,33 +3,31 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Page({ pageInfo, children }) {
-	const {
-		cssClass,
-		pageHeading,
-		pageIntro,
-		metaDescription,
-		metaKeywords,
-		metaTitle,
-	} = pageInfo;
+  const {
+    cssClass,
+    pageHeading,
+    pageIntro,
+    metaDescription,
+    metaKeywords,
+    metaTitle,
+  } = pageInfo;
 
-	return (
-		<div id='Meta' className='meta'>
-			<Head>
-				<title>{metaTitle}</title>
-				<meta name='keywords' content={metaKeywords} />
-				<meta name='description' content={metaDescription} />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<div className={`page ${cssClass}-page`}>
-				<Header title={pageHeading} />
-				<main className='content'>
-					<p className='pPageIntro'>{pageIntro}</p>
-					{children}
-					{/* <!--JavaScript at end of body for optimized loading--> */}
-					<script type='text/javascript' src='js/materialize.min.js'></script>
-				</main>
-				<Footer />
-			</div>
-		</div>
-	);
+  return (
+    <div id="Meta" className="meta">
+      <Head>
+        <title>{metaTitle}</title>
+        <meta name="keywords" content={metaKeywords} />
+        <meta name="description" content={metaDescription} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={`page ${cssClass}-page`}>
+        <Header title={pageHeading} />
+        <main className="content">
+          <p className="pPageIntro">{pageIntro}</p>
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
