c3a4895c86076f0fbdb8c07bdb179a9c6832dcee
Ephraim <admin@ephraimsmith.dev>
2022-01-01 18:59:31 -0600

    quiet unused blog index page

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -6,35 +6,35 @@ import Footer from "@/components/Footer";
 import { PAGE_INFO } from "../../sitedata/page_info";
 
 export default function Archives({ posts }) {
-	const { blog } = PAGE_INFO;
+  const { blog } = PAGE_INFO;
 
-	return (
-		<Page pageInfo={blog}>
-			<div className='page blog-page'>
-				<Header />
-				<main className='content'>
-					<h1>
-						<span className='blue'>Blog</span> Posts
-					</h1>
-					{posts.map((post, index) => {
-						return (
-							<div>
-								<Link href={`/blog/${post.slug}`} passHref>
-									<a>
-										<h3 className='blog__post-'>
-											{post.frontmatter.leadTitle}
-										</h3>
-									</a>
-								</Link>
-								<h2>{post.frontmatter.headingTitle}</h2>
-							</div>
-						);
-					})}
-				</main>
-				<Footer />
-			</div>
-		</Page>
-	);
+  return (
+    <Page pageInfo={blog}>
+      <div className="page blog-page">
+        <Header />
+        <main className="content">
+          {/* <h1>
+            <span className="blue">Blog</span> Posts
+          </h1>
+          {posts.map((post, index) => {
+            return (
+              <div>
+                <Link href={`/blog/${post.slug}`} PassHref>
+                  <a>
+                    <h3 className="blog__post-">
+                      {post.frontmatter.leadTitle}
+                    </h3>
+                  </a>
+                </Link>
+                <h2>{post.frontmatter.headingTitle}</h2>
+              </div>
+            );
+          })} */}
+        </main>
+        <Footer />
+      </div>
+    </Page>
+  );
 }
 
 // export async function getStaticProps() {
