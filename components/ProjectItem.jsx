f6632bdf7856db596d5c33eafade21288d2f98f3
Ephraim <admin@ephraimsmith.dev>
2022-03-18 05:20:40 -0500

    add test img to ProjectItem components

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,19 +1,16 @@
 import Image from "next/image";
 import ProjectLinks from "./ProjectLinks";
 import ProjectTags from "./ProjectTags";
+import marsIMG from "../public/bg_mars-glowing.jpg";
 
 const ProjectItem = ({ project }) => {
 	const { title, imgURI, description, tech, links } = project;
 
 	return (
 		<li className='bg-neutral-700' key={title}>
-			<h3>{title}</h3>
+			<h3 className='text-center'>{title}</h3>
 			<figure>
-				{/* <Image
-          layout='fill'
-          objectFit='contain'
-          src={require("../public/bg_mars-glowing.jpg")}
-        /> */}
+				<Image alt='test image' src={marsIMG} />
 				<figcaption>{description}</figcaption>
 			</figure>
 			<ProjectTags cardTags={tech} />
