4fa0138eb01f9dbf43fae1a3017644d306af20dd
Ephraim <admin@ephraimsmith.dev>
2022-04-04 18:14:21 -0500

    86 social nav - creating Connect page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,9 @@
-import { SOCIAL_NAV } from "../site_data";
-import NavList from "./NavList";
-
-export default function Footer() {
+export const Footer = () => {
 	return (
 		<footer className='border border-green-600'>
-			<NavList nav={SOCIAL_NAV} />
 			<div className='container mx-auto text-center'>
 				&copy; {new Date().getFullYear()} Ephraim Smith | ephraimsmith.dev
 			</div>
 		</footer>
 	);
-}
+};
