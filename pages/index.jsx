70654ec970b495ad671b9df4c5da9f487bedf3f4
Ephraim <admin@ephraimsmith.dev>
2022-04-26 15:41:19 -0500

    fix className attribute in templates

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,21 +10,21 @@ export default function HomePage() {
 	return (
 		<Page pageInfo={home}>
 			<section>
-				<div class='container px-6 py-8 mx-auto'>
-					<div class='items-center lg:flex'>
-						<div class='lg:w-1/2'>
-							<h2 class='text-3xl font-bold text-gray-800 dark:text-gray-100'>
+				<div className='container px-6 py-8 mx-auto'>
+					<div className='items-center lg:flex'>
+						<div className='lg:w-1/2'>
+							<h2 className='text-3xl font-bold text-gray-800 dark:text-gray-100'>
 								learn. build. document.
 							</h2>
 
-							<p class='mt-4 text-gray-500 dark:text-gray-400 lg:max-w-md'>
+							<p className='mt-4 text-gray-500 dark:text-gray-400 lg:max-w-md'>
 								Hi I am jane , software engineer , Lorem ipsum, dolor sit amet
 								consectetur adipisicing elit. Illum in sed non alias, fugiat,
 								commodi nemo ut fugit corrupti dolorem sequi ex veniam
 								consequuntur id, maiores beatae ipsa omnis aliquam?
 							</p>
 
-							<div class='flex items-center mt-6 -mx-2'>
+							<div className='flex items-center mt-6 -mx-2'>
 								<Link href='https://github.com/ephbuilding' passHref>
 									<a target='_blank'>
 										<FaGithub />
@@ -48,11 +48,11 @@ export default function HomePage() {
 							</div>
 						</div>
 
-						<div class='mt-8 lg:mt-0 lg:w-1/2'>
-							<div class='flex items-center justify-center lg:justify-end'>
-								<div class='max-w-lg'>
+						<div className='mt-8 lg:mt-0 lg:w-1/2'>
+							<div className='flex items-center justify-center lg:justify-end'>
+								<div className='max-w-lg'>
 									<img
-										class='object-cover object-center w-full h-64 rounded-md shadow'
+										className='object-cover object-center w-full h-64 rounded-md shadow'
 										src='https://images.unsplash.com/photo-1484399172022-72a90b12e3c1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
 										alt=''
 									/>
