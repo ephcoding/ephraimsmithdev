defe6d1695453a0a4f32f02116abdfed35dcb873
Ephraim <admin@ephraimsmith.dev>
2022-05-03 12:51:14 -0500

    add article tag styles

--- components/ArticlePreviewCard.jsx
+++ components/ArticlePreviewCard.jsx
@@ -1,4 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
+import {
+	SiPostgresql,
+	SiNodedotjs,
+	SiJavascript,
+	SiTypescript,
+} from "react-icons/si";
 
 export const ArticlePreviewCard = ({ article }) => {
 	const {
@@ -6,15 +13,47 @@ export const ArticlePreviewCard = ({ article }) => {
 		frontmatter: { cover_image, title, sub_title, tag, date },
 	} = article;
 
-	return (
-		<div className="relative bg-[url('/bg_mars-glowing.jpg')] bg-center bg-cover h-[250px] shadow-black">
-			<div className='p-4 bg-neutral-800/50 h-full'>
-				<h3 className='uppercase font-bold'>{title}</h3>
-				<h2 className='card_subtitle'>{sub_title}</h2>
+	const tagMap = {
+		js: {
+			bg_color: "from-js",
+			icon: SiJavascript,
+		},
+		node: {
+			bg_color: "from-node",
+			icon: SiNodedotjs,
+		},
+		pg: {
+			bg_color: "from-pg",
+			icon: SiPostgresql,
+		},
+		ts: {
+			bg_color: "from-ts",
+			icon: SiTypescript,
+		},
+	};
 
-				<div className='absolute bottom-0 right-0 overflow-hidden h-48 w-48'>
-					<div className='bg-gradient-to-r from-green-300/20 to-green-300/50 h-72 w-72 absolute bottom-0 -right-24 rotate-45 origin-bottom-left'></div>
-				</div>
+	// [?] why doesn't THIS work:
+	// ((const tagIcon = tagMap[tag].icon;))
+	// but this DOES??
+	const thisTag = tagMap[tag];
+
+	return (
+		<div
+			className={`relative p-4 overflow-hidden h-56 shadow-white shadow-md rounded bg-neutral-800 mb-3`}
+		>
+			{/* BACKGROUND IMAGE */}
+
+			<h3 className='uppercase font-bold'>{title}</h3>
+			<h2 className='card_subtitle'>{sub_title}</h2>
+
+			<div className='absolute top-0 right-0 overflow-hidden h-24 w-24'>
+				<div
+					className={`bg-gradient-to-br ${thisTag.bg_color} to-neutral-700 h-48 w-48 absolute top-0 -right-24 origin-top-left -rotate-45 border-2 border-white`}
+				></div>
+			</div>
+
+			<div className='absolute top-4 right-4'>
+				<thisTag.icon size={25} color='white' />
 			</div>
 		</div>
 	);
