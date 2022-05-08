3e9d6a7ca46478c2a3772c5cfe9739b91686ba31
Ephraim <admin@ephraimsmith.dev>
2022-05-08 15:44:15 -0500

    integrate post preview img

--- components/BlogPostPreviewCard.jsx
+++ components/BlogPostPreviewCard.jsx
@@ -1,38 +1,40 @@
 import PropTypes from "prop-types";
+import Image from "next/image";
 import Link from "next/link";
 
-export const BlogPostPreviewCard = ({ blog_post, compact }) => {
+export const BlogPostPreviewCard = ({
+	blog_post: { slug, frontmatter },
+	compact,
+}) => {
 	return (
 		<div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
-			{/* {!compact && (
+			{!compact && (
 				<Image
-					src={blog_post.frontmatter.cover_image}
-					alt=''
+					src={frontmatter.cover_image}
+					alt='preview card image'
 					height={420}
 					width={600}
 					className='mb-4 rounded'
 				/>
-			)} */}
+			)}
 			<div className='flex justify-between items-center'>
-				<span className='font-light text-gray-600'>
-					{blog_post.frontmatter.date}
-				</span>
-				<TechTag>{blog_post.frontmatter.category}</TechTag>
-				<ProjectTag>{blog_post.frontmatter.category}</ProjectTag>
+				<span className='font-light text-gray-600'>{frontmatter.date}</span>
+				<TechTag>{frontmatter.category}</TechTag>
+				<ProjectTag>{frontmatter.category}</ProjectTag>
 			</div>
 
 			<div className='mt-2'>
-				<Link href={`/blog/${blog_post.slug}`}>
+				<Link href={`/blog/${slug}`}>
 					<a className='text-2xl text-gray-700 font-bold hover:underline'>
-						{blog_post.frontmatter.title}
+						{frontmatter.title}
 					</a>
 				</Link>
-				<p className='mt-2 text-gray-600'>{blog_post.frontmatter.excerpt}</p>
+				<p className='mt-2 text-gray-600'>{frontmatter.excerpt}</p>
 			</div>
 
 			{!compact && (
 				<div className='flex justify-between items-center mt-6'>
-					<Link href={`/blog/${blog_post.slug}`}>
+					<Link href={`/blog/${slug}`}>
 						<a className='text-gray-900 hover:text-blue-600'>Read More</a>
 					</Link>
 				</div>
