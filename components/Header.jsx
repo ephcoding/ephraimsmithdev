a05a41a0448442e327a33c707bc2545d9084ad8c
Ephraim <admin@ephraimsmith.dev>
2022-05-08 14:36:50 -0500

    refactor Header into smaller components

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,43 +5,61 @@ export const Header = () => {
 	return (
 		<header className='bg-gray-900 text-gray-100 shadow w-full'>
 			<div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
-				<Link href='/'>
-					<a className='flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0'>
-						<Image
-							src='/images/eph-icon-1000.png'
-							width={40}
-							height={40}
-							alt='logo'
-						/>
-						<span className='ml-3 text-xl'>EPHRAIMSMITH.DEV</span>
-					</a>
-				</Link>
-				<nav className='flex flex-wrap md:w-4/5 items-center justify-end text-base md:ml-auto'>
-					<Link href='/blog'>
-						<a className='mx-5 cursor-pointer uppercase hover:text-indigo-300'>
-							blog
-						</a>
-					</Link>
-					<Link href='/about'>
-						<a className='mx-5 cursor-pointer uppercase hover:text-indigo-300'>
-							about
-						</a>
-					</Link>
-				</nav>
+				<HeaderBranding />
+				<HeaderNav />
 			</div>
 		</header>
 	);
 };
 
-const Logo = () => {
-	return <div></div>;
+const HeaderBranding = () => {
+	return (
+		<Link href='/'>
+			<a className='flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0'>
+				<HeaderBrandingIcon />
+
+				<span className='ml-3 text-xl'>EPHRAIMSMITH.DEV</span>
+			</a>
+		</Link>
+	);
 };
-const Branding = () => {
-	return <div></div>;
+
+const HeaderBrandingIcon = () => {
+	return (
+		<Image
+			src='/images/eph-icon-1000.png'
+			width={40}
+			height={40}
+			alt='ephraimsmith.dev icon'
+		/>
+	);
 };
-const MainNav = () => {
-	return <div></div>;
+
+const HeaderNav = () => {
+	const navItems = [
+		{
+			name: "blog",
+			href: "/",
+		},
+		{
+			name: "projects",
+			href: "/projects",
+		},
+	];
+
+	return (
+		<nav className='flex flex-wrap md:w-4/5 items-center justify-end text-base md:ml-auto'>
+			{navItems.map((item) => (
+				<HeaderNavItem navItem={item} />
+			))}
+		</nav>
+	);
 };
-const MainNavItem = () => {
-	return <div></div>;
+
+const HeaderNavItem = ({ navItem: { name, href } }) => {
+	return (
+		<Link href={href}>
+			<a className='mx-5 cursor-pointer  hover:text-indigo-300'>{name}</a>
+		</Link>
+	);
 };
