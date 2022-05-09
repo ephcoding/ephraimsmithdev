dbc291f452d3037bc4cba36c2e699b4d88e3d591
Ephraim <admin@ephraimsmith.dev>
2022-05-09 00:12:16 -0500

    add dynamic route files

---
+++ pages/blog/tag/[posts-by-tag-index].js
@@ -0,0 +1,9 @@
+import PropTypes from "prop-types";
+
+const SearchPostsByTagPage = (props) => {
+	return <div>SearchPostsByTagPage</div>;
+};
+
+SearchPostsByTagPage.propTypes = {};
+
+export default SearchPostsByTagPage;
