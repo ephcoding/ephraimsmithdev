1aeeaa27f29452a6419852bd91f31a4cd5d58934
Ephraim <admin@ephraimsmith.dev>
2022-05-09 00:49:15 -0500

    setup blog route index

---
+++ pages/blog/index.js
@@ -0,0 +1,5 @@
+import { getStaticProps } from "./tag/[posts-by-tag-index]";
+import SearchPostsByTagPage from "./tag/[posts-by-tag-index]";
+
+export { getStaticProps };
+export default SearchPostsByTagPage;
