e085a42ea5bb193576c9e4d605fadd4b218cbc0b
Ephraim <admin@ephraimsmith.dev>
2022-05-08 19:28:00 -0500

    styling preview cards

--- components/BlogPostPreviewCard.jsx
+++ components/BlogPostPreviewCard.jsx
@@ -2,117 +2,121 @@ import PropTypes from "prop-types";
 import Image from "next/image";
 import Link from "next/link";
 import {
-	SiJavascript,
-	SiJest,
-	SiNodedotjs,
-	SiPostgresql,
-	SiReact,
-	SiTypescript,
+  SiJavascript,
+  SiJest,
+  SiNodedotjs,
+  SiPostgresql,
+  SiReact,
+  SiTypescript,
 } from "react-icons/si";
 
-export const BlogPostPreviewCard = ({
-	blog_post: { slug, frontmatter },
-	compact,
-}) => {
-	const colorMap = {
-		javascript: "border-[#f7df1e]",
-		jest: "border-[#cc4615]",
-		nodejs: "border-[#66cc33]",
-		postgresql: "border-[#336791]",
-		react: "border-[#61DAFB]",
-		reactnative: "border-[#61DAFB]",
-		typescript: "border-[#007ACC]",
-	};
+export const BlogPostPreviewCard = ({ blog_post: { slug, frontmatter } }) => {
+  const colorMap = {
+    javascript: "border-[#f7df1e]",
+    jest: "border-[#cc4615]",
+    nodejs: "border-[#66cc33]",
+    postgresql: "border-[#336791]",
+    react: "border-[#61DAFB]",
+    reactnative: "border-[#61DAFB]",
+    typescript: "border-[#007ACC]",
+  };
 
-	const borderColor = colorMap[frontmatter.tag.toLowerCase()];
+  const borderColor = colorMap[frontmatter.tag.toLowerCase()];
 
-	return (
-		<div
-			className={`w-full px-10 py-6 shadow-md shadow-white mt-6 rounded-lg border-t-4 ${borderColor}`}
-		>
-			<BackgroundImage imgURL={frontmatter.cover_image} tag={frontmatter.tag} />
+  return (
+    <div
+      className={`w-full px-10 py-6 shadow-md shadow-white mt-6 rounded-lg border-t-8 ${borderColor}`}
+    >
+      <PreviewCardCoverImage
+        cover_image={frontmatter.cover_image}
+        tag={frontmatter.tag}
+      />
 
-			<div className='flex justify-between items-center'>
-				<span className='font-light'>{frontmatter.date}</span>
-				<ProjectTag>{frontmatter.project}</ProjectTag>
-			</div>
+      <div className="flex justify-between items-center">
+        <span className="font-light">{frontmatter.date}</span>
+        <PreviewCardProjectTag>{frontmatter.project}</PreviewCardProjectTag>
+      </div>
 
-			<BodyText title={frontmatter.title} excerpt={frontmatter.excerpt} />
-			<ReadItButton slug={slug} />
-		</div>
-	);
+      <PreviewCardTitles
+        title={frontmatter.title}
+        excerpt={frontmatter.excerpt}
+      />
+      <PreviewCardReadButton slug={slug} />
+    </div>
+  );
 };
 
-const BackgroundImage = ({ color, compact, imgURL, tag }) => {
-	const iconMap = {
-		javascript: SiJavascript,
-		jest: SiJest,
-		nodejs: SiNodedotjs,
-		postgresql: SiPostgresql,
-		react: SiReact,
-		reactnative: SiReact,
-		typescript: SiTypescript,
-	};
+const PreviewCardCoverImage = ({ cover_image, tag }) => {
+  const iconMap = {
+    javascript: SiJavascript,
+    jest: SiJest,
+    nodejs: SiNodedotjs,
+    postgresql: SiPostgresql,
+    react: SiReact,
+    reactnative: SiReact,
+    typescript: SiTypescript,
+  };
 
-	const colorMap = {
-		javascript: "bg-[#f7df1e]",
-		jest: "bg-[#cc4615]",
-		nodejs: "bg-[#66cc33]",
-		postgresql: "bg-[#336791]",
-		react: "bg-[#61DAFB]",
-		reactnative: "bg-[#61DAFB]",
-		typescript: "bg-[#007ACC]",
-	};
+  const colorMap = {
+    javascript: "#f7df1e",
+    jest: "#cc4615",
+    nodejs: "#66cc33",
+    postgresql: "#336791",
+    react: "#61DAFB",
+    reactnative: "#61DAFB",
+    typescript: "#007ACC",
+  };
 
-	const BgIcon = iconMap[tag.toLowerCase()];
-	const bgColor = colorMap[tag.toLowerCase()];
+  const BgIcon = iconMap[tag.toLowerCase()];
+  const iconColor = colorMap[tag.toLowerCase()];
 
-	return (
-		<div
-			className={`relative w-full h-32 flex flex-col items-center justify-center mx-auto mb-4 rounded-lg ${bgColor}`}
-		>
-			<BgIcon size={60} color='text-slate-800' />
-			<p>{tag}</p>
-			{/* <div
-				className={`absolute top-0 right-0 bottom-0 left-0  rounded-lg`}
-			></div> */}
-		</div>
-	);
+  return (
+    <div className={`relative h-32 w-full mb-4`}>
+      <Image
+        src={cover_image}
+        alt="blog post cover image"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        className=" mb-4 rounded"
+      />
+      <div
+        className={`absolute flex flex-col items-center justify-center top-0 right-0 bottom-0 left-0 bg-stone-800/80 rounded`}
+      >
+        <BgIcon size={75} color={iconColor} />
+        {/* <p className="mt-3">{tag}</p> */}
+      </div>
+    </div>
+  );
 };
 
-// const TechTag = ({ border_color, tag }) => {
-// 	return (
-// 		<div className='absolute top-0 right-0 bg-black p-2 rounded-bl rounded-tr'>
-// 			{iconMap[tag.toLowerCase()]}
-// 		</div>
-// 	);
-// };
-
-const ProjectTag = ({ children }) => {
-	return (
-		<div className='px-2 py-1 text-black bg-stone-400 rounded'>{children}</div>
-	);
+const PreviewCardProjectTag = ({ children }) => {
+  return (
+    <div className="px-2 py-1 text-black bg-stone-400 rounded">{children}</div>
+  );
 };
 
-const BodyText = ({ slug, title, excerpt }) => {
-	return (
-		<div className='mt-2'>
-			<Link href={`/blog/${slug}`}>
-				<a className='text-2xl font-bold hover:underline'>{title}</a>
-			</Link>
-			<p className='mt-2'>{excerpt}</p>
-		</div>
-	);
+const PreviewCardTitles = ({ slug, title, excerpt }) => {
+  return (
+    <div className="mt-2">
+      <Link href={`/blog/${slug}`}>
+        <a className="text-2xl font-bold hover:underline">{title}</a>
+      </Link>
+      <p className="mt-2">{excerpt}</p>
+    </div>
+  );
 };
 
-const ReadItButton = ({ compact, slug }) => {
-	return (
-		<div className='mt-6 flex justify-end'>
-			<Link href={`/blog/${slug}`}>
-				<a className='hover:text-blue-600'>Read More</a>
-			</Link>
-		</div>
-	);
+const PreviewCardReadButton = ({ compact, slug }) => {
+  return (
+    <div className="mt-6 flex justify-end">
+      <Link href={`/blog/${slug}`}>
+        <a className="hover:text-blue-600 border-2 border-white px-4 py-2 rounded">
+          Read It!
+        </a>
+      </Link>
+    </div>
+  );
 };
 
 BlogPostPreviewCard.propTypes = {};
--- components/PageWrapper.jsx
+++ components/PageWrapper.jsx
@@ -2,24 +2,24 @@ import Head from "next/head";
 import { Header } from "components";
 
 export const PageWrapper = ({ title, keywords, description, children }) => {
-	return (
-		<div className='bg-stone-800 text-slate-100 min-h-screen'>
-			<Head>
-				<meta name='keywords' content={keywords} />
-				<meta name='description' content={description} />
-				<link rel='icon' href='/favicon.ico' />
-				<title>{title}</title>
-			</Head>
+  return (
+    <div className="bg-stone-800 text-slate-100 min-h-screen">
+      <Head>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+        <title>{title}</title>
+      </Head>
 
-			<Header />
+      <Header />
 
-			<main className='container mx-auto'>{children}</main>
-		</div>
-	);
+      <main className="container px-4 mx-auto">{children}</main>
+    </div>
+  );
 };
 
 PageWrapper.defaultProps = {
-	title: "ephraimsmith.dev",
-	keywords: "build, learn, repeat",
-	excerpt: "overcoming analysis paralysis by shipping code every day",
+  title: "ephraimsmith.dev",
+  keywords: "build, learn, repeat",
+  excerpt: "overcoming analysis paralysis by shipping code every day",
 };
