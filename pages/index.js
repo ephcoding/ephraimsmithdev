96bd9f8390609f228951be358a8b1acf4dabd856
Ephraim <admin@ephraimsmith.dev>
2022-05-10 09:08:10 -0500

    render single BlogPostPreviewCardList component

--- pages/index.js
+++ pages/index.js
@@ -4,48 +4,24 @@ import path from "path";
 import matter from "gray-matter";
 import { sortByDate } from "utils";
 import Link from "next/link";
-import { BlogPostPreviewCard, PageWrapper, SearchBar } from "components";
+import { BlogPostPreviewCardList, PageWrapper, SearchBar } from "components";
 import { getSlugsWithFrMat } from "utils";
 
-export default function HomePage({ blog_posts }) {
+export default function HomePage({ blog_posts: cachedBlogPosts }) {
 	const [searchResults, setSearchResults] = useState([]);
+
+	const blogPostsToDisplay =
+		searchResults.length > 0 ? searchResults : cachedBlogPosts;
 
 	return (
 		<PageWrapper>
 			<SearchBar setResultsCallback={setSearchResults} />
 			<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-				{searchResults.length > 0 ? (
-					<BlogPostSearchResults posts={searchResults} />
-				) : (
-					<CachedBlogPosts posts={blog_posts} />
-				)}
+				<BlogPostPreviewCardList blog_posts={blogPostsToDisplay} />
 			</div>
 		</PageWrapper>
 	);
 }
-
-const CachedBlogPosts = ({ posts }) => {
-	return (
-		<>
-			{posts.length > 0
-				? posts.map((post, index) => (
-						<BlogPostPreviewCard key={index} blog_post={post} />
-				  ))
-				: "No cached blog posts.."}
-		</>
-	);
-};
-const BlogPostSearchResults = ({ posts }) => {
-	return (
-		<>
-			{posts.length > 0
-				? posts.map((post, index) => (
-						<BlogPostPreviewCard key={index} blog_post={post} />
-				  ))
-				: "No posts matching that search..."}
-		</>
-	);
-};
 
 export async function getStaticProps() {
 	const fileNames = fs.readdirSync(path.join("blog_posts"));
