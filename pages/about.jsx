6a21d69604e947f76c0f0471c463ee2ebb5cdbba
Ephraim <admin@ephraimsmith.dev>
2022-05-10 09:07:40 -0500

    create AboutPage component

---
+++ pages/about.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+import { PageWrapper } from "components";
+
+const AboutPage = (props) => {
+	return (
+		<PageWrapper>
+			<h1>about</h1>
+		</PageWrapper>
+	);
+};
+
+AboutPage.propTypes = {};
+
+export default AboutPage;
