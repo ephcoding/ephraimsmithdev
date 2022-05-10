6c90b81f2eaf978acb35f22cba39f399e51a564e
Ephraim <admin@ephraimsmith.dev>
2022-05-10 12:50:10 -0500

    extend theme colors with icon brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				css: "#264de4 ",
+				graphql: "#E10098",
+				html: "#E34E26",
+				javascript: "#f7df1e",
+				jest: "#cc4615",
+				nextjs: "#fff",
+				nodejs: "#66cc33",
+				postgresql: "#336791",
+				react: "#61DAFB",
+				reactnative: "#61DAFB",
+				tailwindcss: "#06B6D4",
+				tensorflow: "#FF6F00",
+				typescript: "#007ACC",
+			},
+		},
 	},
 	plugins: [],
 };
