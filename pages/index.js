faf850f23e7ba7dc09f014812b6ce1f2954aba35
Ephraim <admin@ephraimsmith.dev>
2022-05-10 07:08:37 -0500

    list searchResults state to HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
@@ -7,18 +8,42 @@ import { BlogPostPreviewCard, PageWrapper, SearchBar } from "components";
 import { getSlugsWithFrMat } from "utils";
 
 export default function HomePage({ blog_posts }) {
+	const [searchResults, setSearchResults] = useState([]);
+
 	return (
 		<PageWrapper>
+			<SearchBar setResultsCallback={setSearchResults} />
 			<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-				{blog_posts
-					? blog_posts.map((blogPost, index) => (
-							<BlogPostPreviewCard key={index} blog_post={blogPost} />
-					  ))
-					: "that definitely wasn't supposed to happen..."}
+				{/* <CachedBlogPosts posts={blog_posts} /> */}
+				{/* <BlogPostSearchResults posts={results} /> */}
 			</div>
 		</PageWrapper>
 	);
 }
+
+const CachedBlogPosts = ({ posts }) => {
+	return (
+		<>
+			{posts.length > 0
+				? posts.map((post, index) => (
+						<BlogPostPreviewCard key={index} blog_post={post} />
+				  ))
+				: "No cached blog posts.."}
+		</>
+	);
+};
+const BlogPostSearchResults = ({ posts }) => {
+	console.log(posts);
+	// if (posts.length === 0) return <></>;
+
+	return (
+		<>
+			{/* {posts.map((post, index) => (
+				<BlogPostPreviewCard key={index} blog_post={post} />
+			))} */}
+		</>
+	);
+};
 
 export async function getStaticProps() {
 	const fileNames = fs.readdirSync(path.join("blog_posts"));
@@ -31,11 +56,11 @@ export async function getStaticProps() {
 			"utf-8"
 		);
 
-		const { data: frontmatter } = matter(blogPostContent);
+		const { data: blogPostMeta } = matter(blogPostContent);
 
 		return {
-			blogPostSlug,
-			frontmatter,
+			blog_post_slug: blogPostSlug,
+			blog_post_meta: blogPostMeta,
 		};
 	});
 
