25eb9e0f188d2563d190d52c166c09abe23ac196
Ephraim <admin@ephraimsmith.dev>
2022-06-03 15:13:37 -0500

    update section bg colors

--- site_data/section-data.js
+++ site_data/section-data.js
@@ -1,84 +1,84 @@
 export const SECTION_DATA = [
-  {
-    bg_color: "bg-red-500",
-    heading: "Who Am I",
-    content: (
-      <ul>
-        <li>Skydives: 127</li>
-        <li>Bungee Jumps: 2</li>
-        <li>Miles on Motorcycles: 30,000 +</li>
-      </ul>
-    ),
-  },
-  {
-    bg_color: "bg-stone-400",
-    heading: "Education",
-    content: (
-      <ul>
-        <li>
-          <p>Bowling Green State University</p>
-          <p>Construction Management & Technology</p>
-        </li>
-        <li>
-          <p>Tech Elevator</p>
-          <p>Fullstack Java Bootcamp</p>
-        </li>
-        <li>
-          <p>FreeCodeCamp</p>
-          <p>Responsive Web Design</p>
-        </li>
-        <li>
-          <p>Tech Elevator</p>
-          <p>JavaScript Algorithms & Data Structures</p>
-        </li>
-        <li>
-          <p>FreeCodeCamp</p>
-          <p>Front End Development Libraries</p>
-        </li>
-      </ul>
-    ),
-  },
-  {
-    bg_color: "bg-green-500",
-    heading: "Experience",
-    content: (
-      <ul>
-        <li>
-          <p>Aderant</p>
-          <ul>
-            <li>QA Engineer</li>
-            <li>SDET</li>
-            <li>R&D Developer</li>
-            <li>Internal Tools Developer</li>
-          </ul>
-        </li>
-        <li>
-          <p>Technical Documentation Page</p>
-        </li>
-      </ul>
-    ),
-  },
-  {
-    bg_color: "bg-blue-500",
-    heading: "Projects",
-    content: (
-      <ul>
-        <li>
-          <h2>Tornado Action</h2>
-          <li>Real-Time Tornado Alerts</li>
-          <li>Daily Severe Weather Outlooks</li>
-        </li>
-        <li>
-          <h2>Relative Income</h2>
-          <li>True net-income calculator</li>
-          <li>Financial awareness</li>
-        </li>
-      </ul>
-    ),
-  },
-  {
-    bg_color: "bg-orange-500",
-    heading: "Connect With Me",
-    content: "Connect with me section",
-  },
+	{
+		bg_color: "bg-stone-200",
+		heading: "about me",
+		content: (
+			<ul>
+				<li>Skydives: 127</li>
+				<li>Bungee Jumps: 2</li>
+				<li>Miles on Motorcycles: 30,000 +</li>
+			</ul>
+		),
+	},
+	{
+		bg_color: "bg-stone-900",
+		heading: "education",
+		content: (
+			<ul>
+				<li>
+					<p>Bowling Green State University</p>
+					<p>Construction Management & Technology</p>
+				</li>
+				<li>
+					<p>Tech Elevator</p>
+					<p>Fullstack Java Bootcamp</p>
+				</li>
+				<li>
+					<p>FreeCodeCamp</p>
+					<p>Responsive Web Design</p>
+				</li>
+				<li>
+					<p>FreeCodeCamp</p>
+					<p>JavaScript Algorithms & Data Structures</p>
+				</li>
+				<li>
+					<p>FreeCodeCamp</p>
+					<p>Front End Development Libraries</p>
+				</li>
+			</ul>
+		),
+	},
+	{
+		bg_color: "bg-stone-200",
+		heading: "experience",
+		content: (
+			<ul>
+				<li>
+					<p>Aderant</p>
+					<ul>
+						<li>QA Engineer</li>
+						<li>SDET</li>
+						<li>R \& D Developer</li>
+						<li>Internal Tools Developer</li>
+					</ul>
+				</li>
+				<li>
+					<p>Technical Documentation Page</p>
+				</li>
+			</ul>
+		),
+	},
+	{
+		bg_color: "bg-stone-900",
+		heading: "projects",
+		content: (
+			<ul>
+				<li>
+					<h2>Tornado Action</h2>
+					<li>Real-Time Tornado Alerts</li>
+					<li>Daily Severe Weather Outlooks</li>
+				</li>
+				<li>
+					<h2>Relative Income</h2>
+					<li>True net-income calculator</li>
+					<li>Financial awareness</li>
+				</li>
+			</ul>
+		),
+	},
+	{
+		bg_color: "bg-stone-200",
+		heading: "connect",
+		content: "Connect with me section",
+	},
 ];
