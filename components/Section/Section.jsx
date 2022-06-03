c49edbd616f7c8ffe0d743d596941b666496138b
Ephraim <admin@ephraimsmith.dev>
2022-06-03 14:02:47 -0500

    add children prop

--- components/Section/Section.jsx
+++ components/Section/Section.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-export const Section = ({ section_data }) => {
-  const { bg_color, heading, content } = section_data;
+export const Section = ({ section_data, children }) => {
+	const { bg_color, heading, content } = section_data;
 
-  return (
-    <section className={`${bg_color} min-h-[50vh]`}>
-      <div className="p-4">
-        <h2 className="text-2xl font-bold">{heading}</h2>
-        {content}
-      </div>
-    </section>
-  );
+	return (
+		<section className={`${bg_color} min-h-[50vh]`}>
+			<div className='p-4'>
+				<h2 className='text-2xl font-bold'>{heading}</h2>
+				{children}
+			</div>
+		</section>
+	);
 };
