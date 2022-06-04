6c9a1f5220020334954cfabdb29eafc841c11e8d
Ephraim <admin@ephraimsmith.dev>
2022-06-04 11:47:40 -0500

    clean

--- components/Section/Section.jsx
+++ components/Section/Section.jsx
@@ -6,7 +6,7 @@ export const Section = ({ section_data, children }) => {
 	return (
 		<section className={`${bg_color} min-h-[50vh]`}>
 			<div className='p-4'>
-				<h2 className='text-4xl font-bold'>{heading}</h2>
+				<h2 className='text-4xl font-bold text-center'>{heading}</h2>
 				{children}
 			</div>
 		</section>
