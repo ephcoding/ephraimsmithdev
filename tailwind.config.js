8e015174d827c7dcc88bfd03c9dbb21f64ac142f
Ephraim <admin@ephraimsmith.dev>
2022-06-20 16:35:08 -0500

    clean up configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,11 @@
 module.exports = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+		"pages/**/*.{js,ts,jsx,tsx}",
+		"components/**/*.{js,ts,jsx,tsx}",
+		"site_data/**/*.{js}",
 	],
 	theme: {
-		extend: {
-			colors: {
-				css: "#264de4 ",
-				graphql: "#E10098",
-				html: "#E34E26",
-				javascript: "#f7df1e",
-				jest: "#cc4615",
-				nextjs: "#fff",
-				nodejs: "#66cc33",
-				postgresql: "#336791",
-				react: "#61DAFB",
-				reactnative: "#61DAFB",
-				tailwindcss: "#06B6D4",
-				tensorflow: "#FF6F00",
-				typescript: "#007ACC",
-			},
-		},
+		extend: {},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 };
