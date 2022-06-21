0e046ef1e90e454dd932f6ea9f3f37dae7cbd9fb
Ephraim <admin@ephraimsmith.dev>
2022-06-21 06:01:56 -0500

    update SITE_DATA: projects

--- src/site-data/projects.js
+++ src/site-data/projects.js
@@ -4,115 +4,6 @@ const EPHCODING_GH_PAGES = "https://ephcoding.github.io";
 const MODEVX_GH_REPO = "https://github.com/modevx";
 const MODEVX_GH_PAGES = "https://modevx.github.io";
 
-export const FCC_PROJECTS = [
-	{
-		certificate: "Responsive Web Design",
-		certificate_projects: [
-			{
-				name: "Tribute Page",
-				code_href: `${EPHCODING_GH_REPO}/fcc-tribute-page`,
-				live_href: `${GH_PAGES}/fcc-tribute-page`,
-			},
-			{
-				name: "Survey Form",
-				code_href: `${EPHCODING_GH_REPO}/fcc-survey-form`,
-				live_href: `${GH_PAGES}/fcc-survey-form`,
-			},
-			{
-				name: "Product Landing Page",
-				code_href: `${EPHCODING_GH_REPO}/fcc-product-landing-page`,
-				live_href: `${GH_PAGES}/fcc-product-landing-page`,
-			},
-			{
-				name: "Technical Documentation Page",
-				code_href: `${EPHCODING_GH_REPO}/fcc-technical-documentation-page`,
-				live_href: `${GH_PAGES}/fcc-technical-documentation-page`,
-			},
-			{
-				name: "Portfolio Page",
-				code_href: `${EPHCODING_GH_REPO}/fcc-portfolio-page`,
-				live_href: `${GH_PAGES}/fcc-technical-documentation-page`,
-			},
-		],
-	},
-	{
-		certificate: "JavaScript Algorithms and Data Structures",
-		certificate_projects: [
-			{
-				name: "Palindrome",
-				code_href: `${EPHCODING_GH_REPO}/fcc-palindrome-checker`,
-				live_href: `${GH_PAGES}/fcc-palindrome-checker`,
-			},
-			{
-				name: "Roman Numeral Converter",
-				code_href: `${EPHCODING_GH_REPO}/fcc-roman-numeral-converter`,
-				live_href: `${GH_PAGES}/fcc-roman-numeral-converter`,
-			},
-			{
-				name: "Caesars Cipher",
-				code_href: `${EPHCODING_GH_REPO}/fcc-caesars-cipher`,
-				live_href: `${GH_PAGES}/fcc-caesars-cipher`,
-			},
-			{
-				name: "Telephone Number Validator",
-				code_href: `${EPHCODING_GH_REPO}/fcc-telephone-number-validator`,
-				live_href: `${GH_PAGES}/fcc-telephone-number-validator`,
-			},
-			{
-				name: "Cash Register",
-				code_href: `${EPHCODING_GH_REPO}/fcc-cash-register`,
-				live_href: `${GH_PAGES}/fcc-cash-register`,
-			},
-		],
-	},
-	{
-		certificate: "Front End Development Libraries",
-		certificate_projects: [
-			{
-				name: "Random Quote Machine",
-				code_href: `${EPHCODING_GH_REPO}/fcc-random-quote-machine`,
-				live_href: `${GH_PAGES}/fcc-random-quote-machine`,
-			},
-			{
-				name: "Markdown Previewer",
-				code_href: `${EPHCODING_GH_REPO}/fcc-markdown-previewer`,
-				live_href: `${GH_PAGES}/fcc-markdown-previewer`,
-			},
-			{
-				name: "Drum Machine",
-				code_href: `${EPHCODING_GH_REPO}/fcc-drum-machine`,
-				live_href: `${GH_PAGES}/fcc-drum-machine`,
-			},
-			{
-				name: "JavaScript Calculator",
-				code_href: `${EPHCODING_GH_REPO}/fcc-js-calculator`,
-				live_href: `${GH_PAGES}/fcc-js-calculator`,
-			},
-			{
-				name: "25 + 5 Clock",
-				code_href: `${EPHCODING_GH_REPO}/fcc-fcc-pomodoro`,
-				live_href: `${GH_PAGES}/fcc-fcc-pomodoro`,
-			},
-		],
-	},
-	{
-		certificate: "Data Visualization",
-		certificate_projects: [
-			{
-				name: "GDP Graph Chart",
-				code_href: `${EPHCODING_GH_REPO}/fcc-bar-chart`,
-				live_href: `${GH_PAGES}/fcc-bar-chart`,
-			},
-		],
-	},
-];
-
-export const MY_APPS = [
-	{ name: "ROVERCAM", description: "", img: "", hrefs: [] },
-	{ name: "SportsWX", description: "", img: "", hrefs: [] },
-	{ name: "Tornado Warned", description: "", img: "", hrefs: [] },
-];
-
 export const PROJECTS = [
 	{
 		name: "Tornado Action",
@@ -130,7 +21,7 @@ export const PROJECTS = [
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
 		img_src: "",
 		hrefs: {
-			code: `${EPHCODING_GH_REPO}`,
+			code: `${MODEVX_GH_REPO}/rovercam`,
 			live_project: "https://rovercamapp.com",
 		},
 	},
@@ -139,33 +30,48 @@ export const PROJECTS = [
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
 		img_src: "",
-		hrefs: { code: `${EPHCODING_GH_REPO}`, live_project: `${GH_PAGES}` },
+		hrefs: {
+			code: `${EPHCODING_GH_REPO}`,
+			live_project: `${EPHCODING_GH_PAGES}`,
+		},
 	},
 	{
 		name: "Drum Kit",
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
 		img_src: "",
-		hrefs: { code: `${EPHCODING_GH_REPO}`, live_project: `${GH_PAGES}` },
+		hrefs: {
+			code: `${EPHCODING_GH_REPO}`,
+			live_project: `${EPHCODING_GH_PAGES}`,
+		},
 	},
 	{
 		name: "Markdown Previewer",
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
 		img_src: "",
-		hrefs: { code: `${EPHCODING_GH_REPO}`, live_project: `${GH_PAGES}` },
+		hrefs: {
+			code: `${EPHCODING_GH_REPO}`,
+			live_project: `${EPHCODING_GH_PAGES}`,
+		},
 	},
 	{
 		name: "Bar Chart Component",
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
 		img_src: "",
-		hrefs: { code: `${EPHCODING_GH_REPO}`, live_project: `${GH_PAGES}` },
+		hrefs: {
+			code: `${EPHCODING_GH_REPO}`,
+			live_project: `${EPHCODING_GH_PAGES}`,
+		},
 	},
 	{
 		name: "Product Landing Page Template",
 		description: "",
 		img_src: "",
-		hrefs: { code: `${EPHCODING_GH_REPO}`, live_project: `${GH_PAGES}` },
+		hrefs: {
+			code: `${EPHCODING_GH_REPO}`,
+			live_project: `${EPHCODING_GH_PAGES}`,
+		},
 	},
 ];
