f16f2213d75b9568f668e77603092b6c30b5ec12
Ephraim <admin@ephraimsmith.dev>
2022-06-21 04:50:38 -0500

    update PageWrapper meta

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -2,28 +2,37 @@ import Head from "next/head";
 import { Header } from "components";
 
 export const PageWrapper = ({ title, children }) => {
-  return (
-    <div className="bg-black text-slate-100 min-h-screen">
-      <Head>
-        <meta
-          name="keywords"
-          content="Ephraim Smith, coding, software development, React Native, React, mobile development, mobile apps, mobile software, web development"
-        />
-        <meta name="description" content="" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="ephraimsmith.dev" />
-        <meta property="og:url" content="https://www.ephraimsmith.dev" />
-        <meta property="og:description" content="" />
-        <meta property="og:image" content="" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@ephcoding" />
-        <title>{title}</title>
-      </Head>
+	return (
+		<div className='bg-black text-slate-100 min-h-screen'>
+			<Head>
+				<meta
+					name='keywords'
+					content='Ephraim Smith, coding, software development, React Native, React, mobile development, mobile apps, mobile software, web development'
+				/>
+				<meta
+					name='description'
+					content="Ephraim Smith's personal website and software development portfolio"
+				/>
+				<link rel='icon' href='/favicon.ico' />
+				<meta property='og:title' content='ephraimsmith.dev' />
+				<meta property='og:url' content='https://www.ephraimsmith.dev' />
+				<meta
+					property='og:description'
+					content="Ephraim Smith's personal website and software development portfolio"
+				/>
+				<meta
+					property='og:image'
+					content='https://ephraimsmith.dev/repo-preview-esdev.png'
+				/>
+				<meta property='og:type' content='website' />
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta name='twitter:creator' content='@ephcoding' />
+				<title>Ephraim Smith</title>
+			</Head>
 
-      <Header />
+			<Header />
 
-      <main className="container px-4 mx-auto">{children}</main>
-    </div>
-  );
+			<main className='container px-4 mx-auto'>{children}</main>
+		</div>
+	);
 };
