2593b5ca460dd0e98c63ccd6bc4d692abb36165b
Ephraim <admin@ephraimsmith.dev>
2022-06-21 04:48:52 -0500

    update components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,2 @@
-export * from "./BlogPostPreviewCardList";
 export * from "./Header";
 export * from "./PageWrapper";
-export * from "./ProjectFilterList";
-export * from "./SearchBar";
-export * from "./TagIconFilterList";
