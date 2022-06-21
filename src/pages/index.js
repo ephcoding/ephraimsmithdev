0c39935ca3c8322f7a39ffe888aa272f7b4cd251
Ephraim <admin@ephraimsmith.dev>
2022-06-21 12:43:12 -0500

    add init project card code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,37 @@
 import Link from "next/link";
 import { PageWrapper } from "components";
+import { PROJECTS } from "site-data";
 
 export default function HomePage() {
-  return <PageWrapper></PageWrapper>;
+	return (
+		<PageWrapper>
+			<section id='projects-section'>
+				{PROJECTS.map(({ name, description, img_src, hrefs }) => (
+					<div className='card card-compact card-bordered bg-base-100 shadow-xl'>
+						<figure>
+							<img src={img_src} alt='Album' />
+						</figure>
+						<div className='card-body'>
+							<h2 className='card-title'>{name}</h2>
+							<p>{description}</p>
+							<div className='card-actions justify-end'>
+								<Link href={hrefs.code}>
+									<a target='_blank'>
+										<button className='btn btn-primary'>code</button>
+									</a>
+								</Link>
+								<Link href={hrefs.live_project}>
+									<a target='_blank'>
+										<button className='btn btn-primary'>see it live</button>
+									</a>
+								</Link>
+							</div>
+						</div>
+					</div>
+				))}
+			</section>
+			<section id='experience-section'></section>
+			<section id='education-section'></section>
+		</PageWrapper>
+	);
 }
