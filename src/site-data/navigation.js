87dcb98255a45672f37a69e44bb96103e016cf57
Ephraim <admin@ephraimsmith.dev>
2022-06-21 06:09:03 -0500

    update SITE_DATA: index & navigation

--- src/site-data/navigation.js
+++ src/site-data/navigation.js
@@ -1,3 +1,7 @@
-export const HEADER_NAV = [{ label: "", tooltip: "", href: "/" }];
+export const HEADER_NAV = [
+	{ label: "projects", tooltip: "Projects", href: "#projects" },
+	{ label: "experience", tooltip: "Experience", href: "#experience" },
+	{ label: "education", tooltip: "Education", href: "#education" },
+];
 
 export const FOOTER_NAV = [{ section: "", hrefs: [] }];
