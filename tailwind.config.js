cc029edc0cea4c5596483d40bb6c4df3d8588b73
Ephraim <admin@ephraimsmith.dev>
2022-06-21 05:58:39 -0500

    add TWCSS Typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@ module.exports = {
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx}",
 		"./src/components/**/*.{js,ts,jsx,tsx}",
-		"./src/site_data/**/*.{js}",
+		"./src/site_data/**/*.js",
 	],
 	theme: {
 		extend: {},
 	},
-	plugins: [require("daisyui")],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
 		styled: true,
 		themes: ["dark", "light"],
