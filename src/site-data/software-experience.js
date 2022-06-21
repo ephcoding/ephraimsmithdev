24d315a7a603be930b1fb974a4756ad62aa37927
Ephraim <admin@ephraimsmith.dev>
2022-06-21 13:49:59 -0500

    add temp default text for dev

--- src/site-data/software-experience.js
+++ src/site-data/software-experience.js
@@ -1,2 +1,30 @@
+// import AderantLogo from "images/aderant-logo-reverse-1024.png";
+// import ModevxLogo from "images/modevx-icon-1024.png";
+
 // TRANSFER INFO OVER FROM RESUME
-export const SOFT_EXPERIENCE = [];
+export const SOFTWARE_EXPERIENCE = [
+	{
+		co_name: "aderant",
+		// logo_src: AderantLogo,
+		from_year: "2019",
+		to_year: "2021",
+		achievements: [
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+		],
+	},
+	{
+		co_name: "modevx",
+		// logo_src: ModevxLogo,
+		from_year: "2021",
+		to_year: "Present",
+		achievements: [
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+		],
+	},
+];
