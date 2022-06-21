83ff6959c00a99a2289df4296a04b5ecbb3a1a57
Ephraim <admin@ephraimsmith.dev>
2022-06-21 12:42:31 -0500

    change theme to winter

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -3,7 +3,7 @@ import { Footer, Hero, Navbar } from "components";
 
 export const PageWrapper = ({ children }) => {
 	return (
-		<div data-theme='halloween'>
+		<div data-theme='winter'>
 			<Head>
 				<meta
 					name='keywords'
