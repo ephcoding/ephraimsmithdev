d88c42c65d302a109e439a03e47084d10e70684c
Ephraim <admin@ephraimsmith.dev>
2022-06-21 04:19:19 -0500

    update eduction site data

--- src/site-data/education.js
+++ src/site-data/education.js
@@ -1,42 +1,42 @@
 export const EDUCATION = [
 	{
 		school: "Bowling Green State University",
-		type: "B.S.",
 		concentration: "Construction Management and Technology",
+		img_src: "",
 	},
 	{
 		school: "International Union of Operating Engineers",
-		type: "Diploma",
 		concentration: "Heavy Equipment Operator",
+		img_src: "",
 	},
 	{
 		school: "Erie County Technical School",
-		type: "3-Year Certificate",
 		concentration: "Construction Trades",
+		img_src: "",
 	},
 	{
 		school: "United States Army",
-		type: "Certificate",
 		concentration: "Basic Combat Training",
+		img_src: "",
 	},
 	{
 		school: "Tech Elevator",
-		type: "Diploma",
 		concentration: "Fullstack Java Development",
+		img_src: "",
 	},
 	{
 		school: "FreeCodeCamp",
-		type: "Certificate",
 		concentration: "Responsive Web Design",
+		img_src: "",
 	},
 	{
 		school: "FreeCodeCamp",
-		type: "Certificate",
 		concentration: "JavaScript Algorithms and Data Structures",
+		img_src: "",
 	},
 	{
 		school: "FreeCodeCamp",
-		type: "Certificate",
 		concentration: "Front End Development Libraries",
+		img_src: "",
 	},
 ];
