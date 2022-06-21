cd5f73376ed229c0112a68908653f8ce33d4ee94
Ephraim <admin@ephraimsmith.dev>
2022-06-21 07:34:51 -0500

    update SITE_DATA: links to use React Icon names

--- src/site-data/link-menus.js
+++ src/site-data/link-menus.js
@@ -1,30 +1,28 @@
-import { FaEnvelope, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-
 export const SITE_NAV = [
-	{ name: "projects", tooltip: "Projects", href: "#projects" },
-	{ name: "experience", tooltip: "Experience", href: "#experience" },
-	{ name: "education", tooltip: "Education", href: "#education" },
+	{ name: "projects", href: "#projects" },
+	{ name: "experience", href: "#experience" },
+	{ name: "education", href: "#education" },
 ];
 
 export const SOCIAL_LINKS = [
 	{
 		name: "email",
 		href: "mailto:ephraim@modevx.com",
-		icon: FaEnvelope,
+		icon_name: "envelope",
 	},
 	{
 		name: "GitHub",
-		href: "https://github.com/ephbuilding",
-		icon: FaGithub,
+		href: "https://github.com/ephcoding",
+		icon_name: "github",
 	},
 	{
 		name: "Twitter",
-		href: "https://twitter.com/ephbuilding",
-		icon: FaTwitter,
+		href: "https://twitter.com/ephcoding",
+		icon_name: "twitter",
 	},
 	{
 		name: "LinkedIn",
-		href: "https://linkedin.com/in/ephbuilding",
-		icon: FaLinkedin,
+		href: "https://linkedin.com/in/ephraimjsmith",
+		icon_name: "linkedin",
 	},
 ];
