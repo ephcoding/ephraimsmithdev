325236668b412b77f6dbd3043a0e392cb2048e84
Ephraim <admin@ephraimsmith.dev>
2022-06-21 14:28:09 -0500

    add Company assets

--- src/site-data/software-experience.js
+++ src/site-data/software-experience.js
@@ -1,23 +1,22 @@
-// import AderantLogo from "images/aderant-logo-reverse-1024.png";
-// import ModevxLogo from "images/modevx-icon-1024.png";
+import AderantLogo from "../../public/images/aderant-logo-reverse-1024.png";
+import ModevxLogo from "../../public/images/modevx-icon-1024.png";
 
 // TRANSFER INFO OVER FROM RESUME
 export const SOFTWARE_EXPERIENCE = [
 	{
 		co_name: "aderant",
-		// logo_src: AderantLogo,
+		logo_src: AderantLogo,
 		from_year: "2019",
 		to_year: "2021",
 		achievements: [
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, totam.",
 		],
 	},
 	{
 		co_name: "modevx",
-		// logo_src: ModevxLogo,
+		logo_src: ModevxLogo,
 		from_year: "2021",
 		to_year: "Present",
 		achievements: [
