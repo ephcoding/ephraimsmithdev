baeb0a92215281e9b8991a2d08ee6fe5171a8da2
Ephraim <admin@ephraimsmith.dev>
2022-06-21 09:41:28 -0500

    create base Navbar

--- src/context/react-icons-context.jsx
+++
@@ -1,9 +0,0 @@
-import { IconContext } from "react-icons";
-
-export const ReactIconsContext = ({ children }) => {
-	return (
-		<IconContext.Provider value={{ color: "bg-accent" }}>
-			{children}
-		</IconContext.Provider>
-	);
-};
