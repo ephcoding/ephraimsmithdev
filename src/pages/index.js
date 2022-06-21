d4e5715ffa4397c615210431abc313403869648f
Ephraim <admin@ephraimsmith.dev>
2022-06-21 13:01:34 -0500

    import ProjectCard in projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,13 @@
 import Link from "next/link";
-import { PageWrapper } from "components";
+import { PageWrapper, ProjectCard } from "components";
 import { PROJECTS } from "site-data";
 
 export default function HomePage() {
 	return (
 		<PageWrapper>
 			<section id='projects-section'>
-				{PROJECTS.map(({ name, description, img_src, hrefs }) => (
-					<div className='card card-compact card-bordered bg-base-100 shadow-xl'>
-						<figure>
-							<img src={img_src} alt='Album' />
-						</figure>
-						<div className='card-body'>
-							<h2 className='card-title'>{name}</h2>
-							<p>{description}</p>
-							<div className='card-actions justify-end'>
-								<Link href={hrefs.code}>
-									<a target='_blank'>
-										<button className='btn btn-primary'>code</button>
-									</a>
-								</Link>
-								<Link href={hrefs.live_project}>
-									<a target='_blank'>
-										<button className='btn btn-primary'>see it live</button>
-									</a>
-								</Link>
-							</div>
-						</div>
-					</div>
+				{PROJECTS.map((projectInfo) => (
+					<ProjectCard project_info={projectInfo} />
 				))}
 			</section>
 			<section id='experience-section'></section>
