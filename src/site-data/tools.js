022245d8875659d3a2fd705c50696cdc7d54c406
Ephraim <admin@ephraimsmith.dev>
2022-06-21 04:19:48 -0500

    update site data: tools

---
+++ src/site-data/tools.js
@@ -0,0 +1,11 @@
+export const TOOLS = [
+	{ name: "Expo/React Native Starter", description: "", img: "", hrefs: [] },
+	{ name: "Fullstack Next.js Starter", description: "", img: "", hrefs: [] },
+	{
+		name: "PostgreSQL/GraphQL API Starter",
+		description: "",
+		img: "",
+		hrefs: [],
+	},
+	{ name: "Web App Starter", description: "", img: "", hrefs: [] },
+];
