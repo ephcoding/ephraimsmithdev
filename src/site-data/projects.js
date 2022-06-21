fc547b35f4cfd7f1213abf6ff703a8e088ef1c40
Ephraim <admin@ephraimsmith.dev>
2022-06-21 12:43:26 -0500

    add quick descriptions to project cards

--- src/site-data/projects.js
+++ src/site-data/projects.js
@@ -8,67 +8,38 @@ export const PROJECTS = [
 	{
 		name: "Tornado Action",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
-		img_src: "",
+			"Stay ahead of mother nature's worst with real-time tornado watches, warnings, and convective outlooks.",
+		img_src: "images/tornado-action.png",
 		hrefs: {
-			code: `${EPHCODING_GH_REPO}`,
+			code: `${EPHCODING_GH_REPO}/tornadoaction`,
 			live_project: "https://www.tornadoaction.com",
 		},
 	},
 	{
 		name: "ROVERCAM",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
-		img_src: "",
+			"Explore the Red Planet through the eyes of the Mars rovers on your Android or iOS phone.",
+		img_src: "images/rovercam.png",
 		hrefs: {
 			code: `${MODEVX_GH_REPO}/rovercam`,
 			live_project: "https://rovercamapp.com",
 		},
 	},
 	{
+		name: "Mobile App Promo Page Template",
+		description:
+			"Single-page website template for promoting or vetting your latest app ideas.",
+		img_src: "images/preview-placeholder.png",
+		hrefs: {
+			code: `${EPHCODING_GH_REPO}`,
+			live_project: `${EPHCODING_GH_PAGES}`,
+		},
+	},
+	{
 		name: "Break It Up",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
-		img_src: "",
-		hrefs: {
-			code: `${EPHCODING_GH_REPO}`,
-			live_project: `${EPHCODING_GH_PAGES}`,
-		},
-	},
-	{
-		name: "Drum Kit",
-		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
-		img_src: "",
-		hrefs: {
-			code: `${EPHCODING_GH_REPO}`,
-			live_project: `${EPHCODING_GH_PAGES}`,
-		},
-	},
-	{
-		name: "Markdown Previewer",
-		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
-		img_src: "",
-		hrefs: {
-			code: `${EPHCODING_GH_REPO}`,
-			live_project: `${EPHCODING_GH_PAGES}`,
-		},
-	},
-	{
-		name: "Bar Chart Component",
-		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magnam provident officiis eaque repudiandae id vitae hic asperiores ex cumque.",
-		img_src: "",
-		hrefs: {
-			code: `${EPHCODING_GH_REPO}`,
-			live_project: `${EPHCODING_GH_PAGES}`,
-		},
-	},
-	{
-		name: "Product Landing Page Template",
-		description: "",
-		img_src: "",
+			"High-focus tasks require dedicated break time.  Give your brain the breaks it needs with Break It Up.",
+		img_src: "images/preview-placeholder.png",
 		hrefs: {
 			code: `${EPHCODING_GH_REPO}`,
 			live_project: `${EPHCODING_GH_PAGES}`,
